e3d33d780e730d36fe8bfc5a237052bd8fc867b7
kostas.kougios <kostas.kougios@googlemail.com>
2011-09-02 22:57:31 +0000

    com.rits => com.googlecode.mapperdao

---
+++ src/main/java/com/googlecode/mapperdao/jdbc/JdbcMap.scala
@@ -0,0 +1,20 @@
+package com.googlecode.mapperdao.jdbc
+import org.joda.time.DateTime
+
+/**
+ * @author kostantinos.kougios
+ *
+ * 2 Aug 2011
+ */
+class JdbcMap(val map: Map[String, _]) {
+	def apply(key: String): Any = map(key)
+	def int(key: String): Int = map(key).asInstanceOf[Int]
+	def long(key: String): Long = map(key).asInstanceOf[Long]
+	def string(key: String): String = map(key).asInstanceOf[String]
+	def datetime(key: String): DateTime = map(key).asInstanceOf[DateTime]
+
+	def size: Int = map.size
+	def isEmpty: Boolean = map.isEmpty
+
+	override def toString = map.toString
+}
---
+++ src/main/java/com/googlecode/mapperdao/jdbc/UpdateResult.scala
@@ -0,0 +1,18 @@
+package com.googlecode.mapperdao.jdbc
+
+/**
+ * a result of a jdbc update
+ *
+ * @author kostantinos.kougios
+ *
+ * 2 Aug 2011
+ */
+class UpdateResult(val rowsAffected: Int)
+
+class UpdateResultWithGeneratedKeys(override val rowsAffected: Int, val keys: Map[String, Any]) extends UpdateResult(rowsAffected) {
+	def intKey(key: String): Int = keys(key).asInstanceOf[Int]
+	def longKey(key: String): Long = keys(key).asInstanceOf[Long]
+	def stringKey(key: String): String = keys(key).asInstanceOf[String]
+
+	override def toString = "UpdateResultWithGeneratedKeys(%d,%s)".format(rowsAffected, keys)
+}
