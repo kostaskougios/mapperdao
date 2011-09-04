17778d1e9cc559e1145d68ec28fd942bab9d63ef
kostas.kougios <kostas.kougios@googlemail.com>
2011-09-04 00:47:10 +0000

    order by clause

--- src/test/java/com/googlecode/mapperdao/ManyToManyQuerySpec.scala
+++ src/test/java/com/googlecode/mapperdao/ManyToManyQuerySpec.scala
@@ -18,6 +18,22 @@ class ManyToManyQuerySpec extends SpecificationWithJUnit {
 	import queryDao._
 	import TestQueries._
 
+	"order by" in {
+		createTables
+		val a = insert(AttributeEntity, Attribute(100, "size", "A"))
+		val b = insert(AttributeEntity, Attribute(101, "size", "B"))
+		val c = insert(AttributeEntity, Attribute(102, "size", "C"))
+		val d = insert(AttributeEntity, Attribute(103, "size", "D"))
+
+		val p1 = insert(ProductEntity, Product(1, "TV 1", Set(a, b)))
+		val p2 = insert(ProductEntity, Product(2, "TV 2", Set(c, d)))
+		val p3 = insert(ProductEntity, Product(3, "TV 3", Set(a, c)))
+		val p4 = insert(ProductEntity, Product(4, "TV 4", Set(d)))
+
+		val result = query(q0OrderBy)
+		result must_== List(p3, p1, p1, p3, p2, p4, p2)
+	}
+
 	"join, 1 condition" in {
 		createTables
 		val a0 = insert(AttributeEntity, Attribute(100, "size", "46'"))
@@ -28,7 +44,7 @@ class ManyToManyQuerySpec extends SpecificationWithJUnit {
 		val p0 = insert(ProductEntity, Product(1, "TV 1", Set(a0, a2)))
 		val p1 = insert(ProductEntity, Product(2, "TV 2", Set(a1, a2)))
 		val p2 = insert(ProductEntity, Product(3, "TV 3", Set(a0, a3)))
-		val p3 = insert(ProductEntity, Product(4, "TV 3", Set(a1, a3)))
+		val p3 = insert(ProductEntity, Product(4, "TV 4", Set(a1, a3)))
 
 		query(q0).toSet must_== Set(p0, p2)
 	}
@@ -43,7 +59,7 @@ class ManyToManyQuerySpec extends SpecificationWithJUnit {
 		val p0 = insert(ProductEntity, Product(1, "TV 1", Set(a0, a2)))
 		val p1 = insert(ProductEntity, Product(2, "TV 2", Set(a1, a2)))
 		val p2 = insert(ProductEntity, Product(3, "TV 3", Set(a0, a3)))
-		val p3 = insert(ProductEntity, Product(4, "TV 3", Set(a1, a3)))
+		val p3 = insert(ProductEntity, Product(4, "TV 4", Set(a1, a3)))
 
 		query(q1).toSet must_== Set(p0, p1, p3)
 	}
@@ -89,6 +105,8 @@ object ManyToManyQuerySpec {
 		def q0 = select from p join (p, p.attributes, a) where a.value === "46'"
 
 		def q1 = select from p join (p, p.attributes, a) where a.value === "50'" or a.value === "black"
+
+		def q0OrderBy = select from p join (p, p.attributes, a) orderBy (a.value, asc, p.id, desc)
 	}
 
 	case class Product(val id: Int, val name: String, val attributes: Set[Attribute])
