e2ee95bff646c5e32fb11ff08cadc969c3b1c32f
kostas.kougios <kostas.kougios@googlemail.com>
2011-10-12 09:35:38 +0000

    memory mapper dao

--- src/main/java/com/googlecode/mapperdao/exceptions/Exceptions.scala
+++ src/main/java/com/googlecode/mapperdao/exceptions/Exceptions.scala
@@ -5,5 +5,7 @@ package com.googlecode.mapperdao.exceptions
  *
  * 31 Aug 2011
  */
-class PersistException(msg: String, cause: Throwable) extends RuntimeException(msg + "\n" + cause.getMessage, cause)
+class PersistException(msg: String, cause: Throwable) extends RuntimeException(msg + "\n" + cause.getMessage, cause) {
+	def this(msg: String) = this(msg, null)
+}
 class QueryException(msg: String, cause: Throwable) extends RuntimeException(msg + "\n" + cause.getMessage, cause)
