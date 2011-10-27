e060794e27522c5fcb880ce8544b9440949b3dbf
kostas.kougios <kostas.kougios@googlemail.com>
2011-10-27 17:36:25 +0000

    Helper class with methods to manipulate collections, useful for web applications

--- src/test/java/com/googlecode/mapperdao/utils/HelpersSpec.scala
+++ src/test/java/com/googlecode/mapperdao/utils/HelpersSpec.scala
@@ -14,7 +14,7 @@ class HelpersSpec extends SpecificationWithJUnit {
 
 	"added (set)" in {
 		val origT1 = T(1)
-		val moded = modified(Set(origT1), Set(T(1), T(2), T(3)))
+		val moded = merge(Set(origT1), Set(T(1), T(2), T(3)))
 		// make sure the set retains origT1 (same instance)
 		moded.filter(_.eq(origT1)).size must_== 1
 		// and make sure the set contains all items
@@ -22,12 +22,12 @@ class HelpersSpec extends SpecificationWithJUnit {
 	}
 
 	"removed (set)" in {
-		modified(Set(T(1)), Set(T(2), T(3))) must_== Set(T(2), T(3))
+		merge(Set(T(1)), Set(T(2), T(3))) must_== Set(T(2), T(3))
 	}
 
 	"added (list)" in {
 		val origT1 = T(1)
-		val moded = modified(List(origT1), List(T(1), T(2), T(3)))
+		val moded = merge(List(origT1), List(T(1), T(2), T(3)))
 		// make sure the set retains origT1 (same instance)
 		moded(0) must beTheSameAs(origT1)
 		// and make sure the set contains all items
@@ -37,7 +37,7 @@ class HelpersSpec extends SpecificationWithJUnit {
 	"added different instance but equals, keep 1 only (list)" in {
 		val origT1 = T(1)
 		val origT1Second = T(1)
-		val moded = modified(List(origT1, origT1Second), List(T(1), T(2), T(3)))
+		val moded = merge(List(origT1, origT1Second), List(T(1), T(2), T(3)))
 		// make sure the set retains origT1 (same instance)
 		moded(0) must beTheSameAs(origT1)
 		// and make sure the set contains all items
@@ -47,7 +47,7 @@ class HelpersSpec extends SpecificationWithJUnit {
 	"added different instance but equals, keep both (list)" in {
 		val origT1 = T(1)
 		val origT1Second = T(1)
-		val moded = modified(List(origT1, origT1Second), List(T(1), T(2), T(1), T(3)))
+		val moded = merge(List(origT1, origT1Second), List(T(1), T(2), T(1), T(3)))
 		// make sure the set retains origT1 (same instance)
 		moded(0) must beTheSameAs(origT1)
 		moded(2) must beTheSameAs(origT1Second)
@@ -56,7 +56,7 @@ class HelpersSpec extends SpecificationWithJUnit {
 	}
 
 	"removed (list)" in {
-		val moded = modified(List(T(1), T(2)), List(T(2), T(3)))
+		val moded = merge(List(T(1), T(2)), List(T(2), T(3)))
 		// and make sure the set contains all items
 		moded must_== List(T(2), T(3))
 	}
