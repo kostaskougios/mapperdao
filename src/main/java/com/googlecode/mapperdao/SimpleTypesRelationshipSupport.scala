efd24c8b69a9659d78181e6360e59101318e3284
kostas.kougios <kostas.kougios@googlemail.com>
2011-11-05 21:34:55 +0000

    impl oneToMany simple types

--- src/main/java/com/googlecode/mapperdao/SimpleTypesRelationshipSupport.scala
+++ src/main/java/com/googlecode/mapperdao/SimpleTypesRelationshipSupport.scala
@@ -27,11 +27,50 @@ class StringEntity(table: String, fkColumn: String, soleColumn: String) extends 
  * int simple type
  */
 case class IntValue(val value: Int) extends SimpleTypeValue[Int, IntValue] {
-	def compareTo(o: IntValue): Int = value - o.value
+	def compareTo(o: IntValue): Int = value.compareTo(o.value)
 }
 
 class IntEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[IntValue](table, classOf[IntValue]) {
 	val value = int(soleColumn, _.value)
 	declarePrimaryKeys(fkColumn, soleColumn)
 	def constructor(implicit m: ValuesMap) = new IntValue(value) with Persisted
+}
+
+/**
+ * long simple type
+ */
+case class LongValue(val value: Long) extends SimpleTypeValue[Long, LongValue] {
+	def compareTo(o: LongValue): Int = value.compare(o.value)
+}
+
+class LongEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[LongValue](table, classOf[LongValue]) {
+	val value = long(soleColumn, _.value)
+	declarePrimaryKeys(fkColumn, soleColumn)
+	def constructor(implicit m: ValuesMap) = new LongValue(value) with Persisted
+}
+
+/**
+ * float simple type
+ */
+case class FloatValue(val value: Float) extends SimpleTypeValue[Float, FloatValue] {
+	def compareTo(o: FloatValue): Int = value.compare(o.value)
+}
+
+class FloatEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[FloatValue](table, classOf[FloatValue]) {
+	val value = float(soleColumn, _.value)
+	declarePrimaryKeys(fkColumn, soleColumn)
+	def constructor(implicit m: ValuesMap) = new FloatValue(value) with Persisted
+}
+
+/**
+ * double simple type
+ */
+case class DoubleValue(val value: Double) extends SimpleTypeValue[Double, DoubleValue] {
+	def compareTo(o: DoubleValue): Int = value.compare(o.value)
+}
+
+class DoubleEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[DoubleValue](table, classOf[DoubleValue]) {
+	val value = double(soleColumn, _.value)
+	declarePrimaryKeys(fkColumn, soleColumn)
+	def constructor(implicit m: ValuesMap) = new DoubleValue(value) with Persisted
 }
--- src/test/resources/sql/OneToManySimpleTypesSpec.postgresql.sql
+++ src/test/resources/sql/OneToManySimpleTypesSpec.postgresql.sql
@@ -1,4 +1,4 @@
-[create-tables]
+[create-tables-string]
 create table Product (
 	id serial not null,
 	name varchar(100) not null,
@@ -9,4 +9,17 @@ create table ProductTags (
 	product_id int not null,
 	tag varchar(30) not null,
 	foreign key (product_id) references Product(id) on delete cascade
+)
+
+[create-tables-int]
+create table ProductI (
+	id serial not null,
+	name varchar(100) not null,
+	primary key (id)
+)
+;
+create table ProductTagsI (
+	producti_id int not null,
+	intTag int not null,
+	foreign key (producti_id) references ProductI(id) on delete cascade
 )
