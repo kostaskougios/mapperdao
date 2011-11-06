4b8f54a59139ee243671e079137e9552b94c86e5
kostas.kougios <kostas.kougios@googlemail.com>
2011-11-06 12:44:46 +0000

    simple type entities to be initialized via their companion objects

--- src/main/java/com/googlecode/mapperdao/SimpleTypesRelationshipSupport.scala
+++ src/main/java/com/googlecode/mapperdao/SimpleTypesRelationshipSupport.scala
@@ -17,10 +17,14 @@ case class StringValue(val value: String) extends SimpleTypeValue[String, String
 	def compareTo(o: StringValue): Int = value.compareTo(o.value)
 }
 
-class StringEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[StringValue](table, classOf[StringValue]) {
+class StringEntity private (table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[StringValue](table, classOf[StringValue]) {
 	val value = string(soleColumn, _.value)
 	declarePrimaryKeys(fkColumn, soleColumn)
 	def constructor(implicit m: ValuesMap) = new StringValue(value) with Persisted
+}
+
+object StringEntity {
+	def oneToMany(table: String, fkColumn: String, soleColumn: String) = new StringEntity(table, fkColumn, soleColumn)
 }
 
 /**
@@ -30,12 +34,15 @@ case class IntValue(val value: Int) extends SimpleTypeValue[Int, IntValue] {
 	def compareTo(o: IntValue): Int = value.compareTo(o.value)
 }
 
-class IntEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[IntValue](table, classOf[IntValue]) {
+class IntEntity private (table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[IntValue](table, classOf[IntValue]) {
 	val value = int(soleColumn, _.value)
 	declarePrimaryKeys(fkColumn, soleColumn)
 	def constructor(implicit m: ValuesMap) = new IntValue(value) with Persisted
 }
 
+object IntEntity {
+	def oneToMany(table: String, fkColumn: String, soleColumn: String) = new IntEntity(table, fkColumn, soleColumn)
+}
 /**
  * long simple type
  */
@@ -43,10 +50,14 @@ case class LongValue(val value: Long) extends SimpleTypeValue[Long, LongValue] {
 	def compareTo(o: LongValue): Int = value.compare(o.value)
 }
 
-class LongEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[LongValue](table, classOf[LongValue]) {
+class LongEntity private (table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[LongValue](table, classOf[LongValue]) {
 	val value = long(soleColumn, _.value)
 	declarePrimaryKeys(fkColumn, soleColumn)
 	def constructor(implicit m: ValuesMap) = new LongValue(value) with Persisted
+}
+
+object LongEntity {
+	def oneToMany(table: String, fkColumn: String, soleColumn: String) = new LongEntity(table, fkColumn, soleColumn)
 }
 
 /**
@@ -56,10 +67,14 @@ case class FloatValue(val value: Float) extends SimpleTypeValue[Float, FloatValu
 	def compareTo(o: FloatValue): Int = value.compare(o.value)
 }
 
-class FloatEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[FloatValue](table, classOf[FloatValue]) {
+class FloatEntity private (table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[FloatValue](table, classOf[FloatValue]) {
 	val value = float(soleColumn, _.value)
 	declarePrimaryKeys(fkColumn, soleColumn)
 	def constructor(implicit m: ValuesMap) = new FloatValue(value) with Persisted
+}
+
+object FloatEntity {
+	def oneToMany(table: String, fkColumn: String, soleColumn: String) = new FloatEntity(table, fkColumn, soleColumn)
 }
 
 /**
@@ -69,8 +84,12 @@ case class DoubleValue(val value: Double) extends SimpleTypeValue[Double, Double
 	def compareTo(o: DoubleValue): Int = value.compare(o.value)
 }
 
-class DoubleEntity(table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[DoubleValue](table, classOf[DoubleValue]) {
+class DoubleEntity private (table: String, fkColumn: String, soleColumn: String) extends SimpleEntity[DoubleValue](table, classOf[DoubleValue]) {
 	val value = double(soleColumn, _.value)
 	declarePrimaryKeys(fkColumn, soleColumn)
 	def constructor(implicit m: ValuesMap) = new DoubleValue(value) with Persisted
+}
+
+object DoubleEntity {
+	def oneToMany(table: String, fkColumn: String, soleColumn: String) = new DoubleEntity(table, fkColumn, soleColumn)
 }
