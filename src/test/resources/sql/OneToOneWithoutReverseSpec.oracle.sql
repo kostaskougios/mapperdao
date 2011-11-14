878f5d3838b677cc1239b56eeee76c9290ac9b9c
kostas.kougios <kostas.kougios@googlemail.com>
2011-11-14 23:15:27 +0000

    impl sql server integration

---
+++ src/test/resources/sql/OneToOneWithoutReverseSpec.oracle.sql
@@ -0,0 +1,23 @@
+[ddl]
+create table Product (
+	id int not null,
+	primary key (id)
+)
+;
+create table Inventory (
+	id int not null,
+	product_id int,
+	stock int not null,
+	primary key (id),
+	foreign key (product_id) references Product(id) on delete cascade
+)
+;
+create or replace trigger cascade_update
+after update of id on Product
+for each row
+begin
+	update Inventory
+	set product_id = :new.id
+	where product_id = :old.id;
+end;
+
