23890fb1e80a68ffd2442094af66976d1325efdd
kostas.kougios <kostas.kougios@googlemail.com>
2011-11-18 17:07:58 +0000

    impl sql server integration

---
+++ src/test/resources/sql/ManyToManyAutoGeneratedSpec.sqlserver.sql
@@ -0,0 +1,42 @@
+[cascade-on]
+create table Product (
+	id int not null identity(1,1),
+	name varchar(100) not null,
+	primary key(id)
+)
+;
+create table Attribute (
+	id int not null identity(1,1),
+	name varchar(100) not null,
+	value varchar(100) not null,
+	primary key(id)
+)
+;
+create table Product_Attribute (
+	product_id int not null,
+	attribute_id int not null,
+	primary key(product_id,attribute_id)
+	,foreign key(product_id) references Product(id) on delete cascade
+	,foreign key(attribute_id) references Attribute(id) on delete cascade
+)
+
+[cascade-off]
+
+create table Product (
+	id int not null identity(1,1),
+	name varchar(100) not null,
+	primary key(id)
+)
+;
+create table Attribute (
+	id int not null identity(1,1),
+	name varchar(100) not null,
+	value varchar(100) not null,
+	primary key(id)
+)
+;
+create table Product_Attribute (
+	product_id int not null,
+	attribute_id int not null,
+	primary key(product_id,attribute_id)
+)
