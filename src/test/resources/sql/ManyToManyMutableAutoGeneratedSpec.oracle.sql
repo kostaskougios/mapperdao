775f2931ba55f87f83968a0f0305f28b5cb29012
kostas.kougios <kostas.kougios@googlemail.com>
2011-11-18 18:45:43 +0000

    fixed specs

--- src/test/resources/sql/ManyToManyMutableAutoGeneratedSpec.oracle.sql
+++ src/test/resources/sql/ManyToManyMutableAutoGeneratedSpec.oracle.sql
@@ -4,12 +4,14 @@ create table Product (
 	name varchar(100) not null,
 	primary key(id)
 )
+;
 create table Attribute (
 	id int not null,
 	name varchar(100) not null,
 	value varchar(100) not null,
 	primary key(id)
 )
+;
 create table Product_Attribute (
 	product_id int not null,
 	attribute_id int not null,
