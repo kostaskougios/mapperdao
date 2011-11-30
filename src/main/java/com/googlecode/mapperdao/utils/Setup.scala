624ee48659b05e185af56021deae782c06403e10
kostas.kougios <kostas.kougios@googlemail.com>
2011-11-30 21:35:32 +0000

    easier Setup

--- src/main/java/com/googlecode/mapperdao/utils/Setup.scala
+++ src/main/java/com/googlecode/mapperdao/utils/Setup.scala
@@ -18,80 +18,32 @@ object Setup {
 	 * sets up a mapperDao and queryDao for the dataSource and entities using postgresql driver
 	 */
 	def postGreSql(dataSource: DataSource, entities: List[Entity[_, _]]): (Jdbc, MapperDao, QueryDao) =
-		{
-			val typeRegistry = TypeRegistry(entities)
-			val typeManager = new DefaultTypeManager
-			val jdbc = Jdbc(dataSource, typeManager)
-			val driver = new PostgreSql(jdbc, typeRegistry)
-			val mapperDao = MapperDao(driver, standardEvents)
-			val queryDao = QueryDao(typeRegistry, driver, mapperDao)
-			(jdbc, mapperDao, queryDao)
-		}
+		apply(Database.PostgreSql, dataSource, entities)
 	/**
 	 * sets up a mapperDao and queryDao for the dataSource and entities using mysql driver
 	 */
 	def mysql(dataSource: DataSource, entities: List[Entity[_, _]]): (Jdbc, MapperDao, QueryDao) =
-		{
-			val typeRegistry = TypeRegistry(entities)
-			val typeManager = new DefaultTypeManager
-			val jdbc = Jdbc(dataSource, typeManager)
-			val driver = new Mysql(jdbc, typeRegistry)
-			val mapperDao = MapperDao(driver, standardEvents)
-			val queryDao = QueryDao(typeRegistry, driver, mapperDao)
-			(jdbc, mapperDao, queryDao)
-		}
+		apply(Database.Mysql, dataSource, entities)
 	/**
 	 * sets up a mapperDao and queryDao for the dataSource and entities using oracle driver
 	 */
 	def oracle(dataSource: DataSource, entities: List[Entity[_, _]]): (Jdbc, MapperDao, QueryDao) =
-		{
-			val typeRegistry = TypeRegistry(entities)
-			val typeManager = new DefaultTypeManager
-			val jdbc = Jdbc(dataSource, typeManager)
-			val driver = new Oracle(jdbc, typeRegistry)
-			val mapperDao = MapperDao(driver, standardEvents)
-			val queryDao = QueryDao(typeRegistry, driver, mapperDao)
-			(jdbc, mapperDao, queryDao)
-		}
+		apply(Database.Oracle, dataSource, entities)
 	/**
 	 * sets up a mapperDao and queryDao for the dataSource and entities using derby driver
 	 */
 	def derby(dataSource: DataSource, entities: List[Entity[_, _]]): (Jdbc, MapperDao, QueryDao) =
-		{
-			val typeRegistry = TypeRegistry(entities)
-			val typeManager = new DefaultTypeManager
-			val jdbc = Jdbc(dataSource, typeManager)
-			val driver = new Derby(jdbc, typeRegistry)
-			val mapperDao = MapperDao(driver, standardEvents)
-			val queryDao = QueryDao(typeRegistry, driver, mapperDao)
-			(jdbc, mapperDao, queryDao)
-		}
+		apply(Database.Derby, dataSource, entities)
 	/**
 	 * sets up a mapperDao and queryDao for the dataSource and entities using sql server driver
 	 */
 	def sqlServer(dataSource: DataSource, entities: List[Entity[_, _]]): (Jdbc, MapperDao, QueryDao) =
-		{
-			val typeRegistry = TypeRegistry(entities)
-			val typeManager = new DefaultTypeManager
-			val jdbc = Jdbc(dataSource, typeManager)
-			val driver = new SqlServer(jdbc, typeRegistry)
-			val mapperDao = MapperDao(driver, standardEvents)
-			val queryDao = QueryDao(typeRegistry, driver, mapperDao)
-			(jdbc, mapperDao, queryDao)
-		}
+		apply(Database.SqlServer, dataSource, entities)
 	/**
 	 * sets up a mapperDao and queryDao for the dataSource and entities using h2 driver
 	 */
 	def h2(dataSource: DataSource, entities: List[Entity[_, _]]): (Jdbc, MapperDao, QueryDao) =
-		{
-			val typeRegistry = TypeRegistry(entities)
-			val typeManager = new DefaultTypeManager
-			val jdbc = Jdbc(dataSource, typeManager)
-			val driver = new H2(jdbc, typeRegistry)
-			val mapperDao = MapperDao(driver, standardEvents)
-			val queryDao = QueryDao(typeRegistry, driver, mapperDao)
-			(jdbc, mapperDao, queryDao)
-		}
+		apply(Database.H2, dataSource, entities)
 
 	def apply(database: Database.DriverConfiguration, dataSource: DataSource, entities: List[Entity[_, _]]): (Jdbc, MapperDao, QueryDao) =
 		apply(database, dataSource, TypeRegistry(entities))
