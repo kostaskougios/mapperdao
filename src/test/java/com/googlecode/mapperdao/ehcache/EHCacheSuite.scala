173cf4d9ee3b7b12c0f25f571ce5a0a919985ea2
kostas.kougios <kostas.kougios@googlemail.com>
2012-03-26 21:30:44 +0000

    impl ehcache caching

--- src/test/java/com/googlecode/mapperdao/ehcache/EHCacheSuite.scala
+++ src/test/java/com/googlecode/mapperdao/ehcache/EHCacheSuite.scala
@@ -1,12 +1,16 @@
 package com.googlecode.mapperdao.ehcache
 
+import scala.actors.Actor.actor
+import scala.actors.Actor.exit
+import scala.actors.Actor.loop
+import scala.actors.Actor.react
+import scala.actors.Actor.sender
 import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FunSuite
-import net.sf.ehcache.CacheManager
 import com.googlecode.mapperdao.CacheOptions
-import scala.actors.Actor._
+import net.sf.ehcache.CacheManager
+import org.scalatest.junit.JUnitRunner
 
 /**
  * @author kostantinos.kougios
@@ -20,34 +24,39 @@ class EHCacheSuite extends FunSuite with ShouldMatchers {
 		val cacheManager = CacheManager.create
 		try {
 			val ehCache = cacheManager.getCache("test")
-			val cache = new CacheUsingEHCache(ehCache)
+			val cache = new CacheUsingEHCache(ehCache) with Locking
 
 			def createActor = actor {
+				var i = 0
+				var errors = 0
 				loop {
 					react {
-						case -1 =>
-							sender ! 'Ok
-							exit()
 						case iteration: Int =>
 							//println(iteration + ":" + Thread.currentThread.getName)
-							cache(List("key1", 1), CacheOptions.OneDay) {
-								10
-							}
+							val key = List("key1", iteration)
+							if (cache(key, CacheOptions.OneDay) {
+								// check if locking works, so no key is calculated twice
+								if (ehCache.get(key) != null) {
+									errors += 1
+									//println("Locking ERROR!")
+								}
+								iteration
+							} != iteration) errors += 1 // check if we get the correct result
+							i += 1
+							if (i % 10 == 0) ehCache.remove(key)
+						case 'exit =>
+							sender ! errors
+							exit()
 					}
 				}
 			}.start
 
-			val a1 = createActor
-			val a2 = createActor
-			val a3 = createActor
+			val actors = for (i <- 1 to 100) yield createActor
 			for (i <- 1 to 10000) {
-				a1 ! i
-				a2 ! i
-				a3 ! i
+				for (actor <- actors) actor ! i % 10
 			}
-			a1 !? -1
-			a2 !? -1
-			a3 !? -1
+			for (actor <- actors)
+				(actor !? 'exit) should be(0)
 		} finally {
 			cacheManager.shutdown()
 		}
