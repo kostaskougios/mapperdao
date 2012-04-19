7c4ed70aa4d80e8ed082e615a933f111e0871c84
kostas.kougios <kostas.kougios@googlemail.com>
2012-04-19 22:51:25 +0000

    lazy loading: implementing infra & many to many

--- src/main/java/com/googlecode/mapperdao/LazyLoadManager.scala
+++ src/main/java/com/googlecode/mapperdao/LazyLoadManager.scala
@@ -2,6 +2,9 @@ package com.googlecode.mapperdao
 
 import com.googlecode.classgenerator.ClassManager
 import com.googlecode.classgenerator.runtime.Args
+import com.googlecode.classgenerator.LazyLoadInstanceFactory
+import org.objenesis.ObjenesisStd
+import com.googlecode.classgenerator.ReflectionManager
 
 /**
  * @author kostantinos.kougios
@@ -9,36 +12,50 @@ import com.googlecode.classgenerator.runtime.Args
  * 18 Apr 2012
  */
 private[mapperdao] class LazyLoadManager {
+
+	import LazyLoadManager._
+
 	private val classManager = new ClassManager
 	type CacheKey = (Class[_], LazyLoad)
 
-	private val classCache = new scala.collection.mutable.HashMap[CacheKey, Class[_]]
+	private val classCache = new scala.collection.mutable.HashMap[CacheKey, LazyLoadInstanceFactory[_]]
 
 	private def proxyFor[PC, T](entity: Entity[PC, T], lazyLoad: LazyLoad, vm: ValuesMap): Class[T] = {
+
 		val clz = entity.clz
 		val relationships = entity.columns.collect {
 			case c: ColumnInfoRelationshipBase[_, _, _, _] => c
 		}
-		val methods = relationships.map(ci =>
-			ci.getterMethod.getOrElse(throw new IllegalStateException("please define getter method for %s".format(ci.column)))
-		).toSet
+		val key = (entity.clz, lazyLoad)
+
+		val factory = classCache.synchronized {
+			classCache.get(key) match {
+				case null =>
+					val methods = relationships.map(ci =>
+						ci.getterMethod.getOrElse(throw new IllegalStateException("please define getter method on entity for %s".format(ci.column)))
+					).toSet
+					val builder = classManager.lazyLoadBuilder(clz, methods).interface[Persisted].get
+					val factory = new LazyLoadInstanceFactory(reflectionManager, builder)
+					classCache.put(key, factory)
+					factory
+				case factory => factory
+			}
+		}
+
 		val methodToAlias = relationships.map { ci =>
 			(ci.getterMethod.get.getName, ci.column.alias)
 		}.toMap
+
+		val instantiator = objenesis.getInstantiatorOf(clz)
+		instantiator.newInstance
 		val f = { args: Args[T, Any] =>
 			val alias = methodToAlias(args.methodName)
 			vm.valueOf(alias)
 		}
-		val key = (entity.clz, lazyLoad)
-		classCache.synchronized {
-			classCache.get(key) match {
-				case None =>
-					val c = classManager.lazyLoad(clz, methods)(f)
-					classCache.put(key, c)
-					c
-				case Some(c) => c.asInstanceOf[Class[T]]
-			}
-		}
 	}
+}
 
+object LazyLoadManager {
+	private val objenesis = new ObjenesisStd
+	private val reflectionManager = new ReflectionManager
 }
