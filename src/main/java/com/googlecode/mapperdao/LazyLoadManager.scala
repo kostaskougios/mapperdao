41a3b3aaff8ad81db1d1ed3ca1a95ac99e8160c4
kostas.kougios <kostas.kougios@googlemail.com>
2012-04-21 23:31:24 +0000

    lazy loading: implementing infra & many to many

--- src/main/java/com/googlecode/mapperdao/LazyLoadManager.scala
+++ src/main/java/com/googlecode/mapperdao/LazyLoadManager.scala
@@ -9,6 +9,8 @@ import com.googlecode.classgenerator.MethodImplementation
 import java.lang.reflect.Method
 
 /**
+ * manages lazy loading of classes
+ *
  * @author kostantinos.kougios
  *
  * 18 Apr 2012
@@ -22,26 +24,37 @@ private[mapperdao] class LazyLoadManager {
 
 	private val classCache = new scala.collection.mutable.HashMap[CacheKey, Class[_]]
 
+	// convert collections returned by mapperdao to actual collections
+	// required by entities
+	private val converters = Map[Class[_], Any => Any](
+		classOf[Set[_]] -> { _.asInstanceOf[List[_]].toSet },
+		classOf[List[_]] -> { _.asInstanceOf[List[_]] },
+		classOf[Array[_]] -> { _.asInstanceOf[List[_]].toArray[Any] }
+	)
+
 	def proxyFor[PC, T](constructed: T with PC, entity: Entity[PC, T], lazyLoad: LazyLoad, vm: ValuesMap): T with PC = {
 
 		val clz = entity.clz
+		// find all relationships that should be proxied
 		val relationships = entity.columns.collect {
 			case c: ColumnInfoRelationshipBase[_, _, _, _] => c
 		}
 		val key = (clz, lazyLoad)
 
+		// get cached proxy class or generate it
 		val proxyClz = classCache.synchronized {
 			classCache.get(key).getOrElse {
 				val methods = relationships.map(ci =>
 					ci.getterMethod.getOrElse(throw new IllegalStateException("please define getter method on entity for %s".format(ci.column)))
 				).toSet
 				if (methods.isEmpty) throw new IllegalStateException("can't lazy load class that doesn't declare any getters for relationships. Entity: %s".format(clz))
-				val proxyClz = createProxyClz(clz, methods);
+				val proxyClz = createProxyClz(clz, methods)
 				classCache.put(key, proxyClz)
 				proxyClz
 			}
 		}
 
+		// prepare the dynamic function
 		val methodToAlias = relationships.map { ci =>
 			(ci.getterMethod.get.getName, ci.column.alias)
 		}.toMap
@@ -50,7 +63,9 @@ private[mapperdao] class LazyLoadManager {
 		val instance = instantiator.newInstance.asInstanceOf[PC with T with MethodImplementation[T]]
 		instance.methodImplementation { args: Args[T, Any] =>
 			val alias = methodToAlias(args.methodName)
-			vm.valueOf(alias)
+			val v = vm.valueOf[Any](alias)
+			val returnType = args.method.getReturnType
+			converters(returnType)(v)
 		}
 		instance
 	}
