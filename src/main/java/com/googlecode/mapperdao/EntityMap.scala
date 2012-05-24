5070dbbfd5c78e9037d5048ca1a4fa53a722eea9
kostas.kougios <kostas.kougios@googlemail.com>
2012-05-24 19:29:59 +0000

    refactoring entitymap

--- src/main/java/com/googlecode/mapperdao/EntityMap.scala
+++ src/main/java/com/googlecode/mapperdao/EntityMap.scala
@@ -11,10 +11,13 @@ import com.googlecode.mapperdao.jdbc.JdbcMap
  *
  * 7 Aug 2011
  */
-private[mapperdao] trait EntityMap extends EntityStack {
+private[mapperdao] trait EntityMap {
 
 	def putMock[T](clz: Class[_], ids: List[Any], entity: T): Unit
 	def get[T](clz: Class[_], ids: List[Any])(f: => Option[T]): Option[T]
+	def down[PC, T, V, FPC, F](o: Type[PC, T], ci: ColumnInfoRelationshipBase[T, V, FPC, F], dv: DatabaseValues): Unit
+	def peek[PC, T, V, FPC, F]: SelectInfo[PC, T, V, FPC, F]
+	def up: Unit
 }
 
 protected case class SelectInfo[PC, T, V, FPC, F](
