cb33f52f1a400df8211de3451b883be14e71cd36
kostas.kougios <kostas.kougios@googlemail.com>
2012-05-27 22:17:35 +0000

    stress testing

--- src/test/java/com/googlecode/mapperdao/stresstesting/StressTestCPUUsage.scala
+++ src/test/java/com/googlecode/mapperdao/stresstesting/StressTestCPUUsage.scala
@@ -3,6 +3,8 @@ package com.googlecode.mapperdao.stresstesting
 import com.googlecode.mapperdao.TypeRegistry
 import com.googlecode.mapperdao.CommonEntities
 import com.googlecode.mapperdao.jdbc.Setup
+import com.googlecode.mapperdao.SelectConfig
+import com.googlecode.mapperdao.LazyLoad
 
 /**
  * @author kostantinos.kougios
@@ -16,18 +18,27 @@ object StressTestCPUUsage extends App {
 	Setup.dropAllTables(jdbc)
 	Setup.commonEntitiesQueries(jdbc).update("product-attribute")
 
+	val data1 = mapperDao.insert(ProductEntity, Product("test", Set(Attribute("colour", "red"), Attribute("colour", "green"))))
+
+	val (f, loops) = args(0) match {
+		case "select" => (select, 100000)
+		case "lazySelect" => (lazySelect, 100000)
+	}
+
+	println("warm up")
+	for (i <- 0 to 1000) {
+		f()
+	}
+
+	println("benchmark")
 	val start = System.currentTimeMillis
-	args(0) match {
-		case "select" => select()
+	for (i <- 0 to 100000) {
+		f()
 	}
 	val dt = System.currentTimeMillis - start
+	println("Dt		: 	%d millis".format(dt))
+	println("throughput	:	%d per sec".format((1000 * loops) / dt))
 
-	println("Dt : %d millis".format(dt))
-
-	def select() {
-		for (i <- 0 to 100000) {
-			mapperDao.select(ProductEntity, i)
-		}
-	}
-
+	def select = () => mapperDao.select(ProductEntity, data1.id)
+	def lazySelect = () => mapperDao.select(SelectConfig(lazyLoad = LazyLoad.all), ProductEntity, data1.id)
 }
