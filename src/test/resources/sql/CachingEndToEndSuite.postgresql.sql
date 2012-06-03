a4af263a4866396446737dba21511accd8ea3e17
kostas.kougios <kostas.kougios@googlemail.com>
2012-06-03 10:32:37 +0000

    1.0.0-rc9

--- src/test/resources/sql/CachingEndToEndSuite.postgresql.sql
+++ src/test/resources/sql/CachingEndToEndSuite.postgresql.sql
@@ -19,3 +19,19 @@ create table Product_Attribute (
 	foreign key (product_id) references Product(id) on update cascade on delete cascade,
 	foreign key (attribute_id) references Attribute(id) on update cascade on delete cascade
 )
+
+[one-to-many]
+create table Person (
+	id serial not null,
+	name varchar(100) not null,
+	primary key (id)
+)
+;
+create table House (
+	id serial not null,
+	address varchar(100) not null,
+	person_id int not null,
+	primary key (id),
+	constraint FK_House_Person foreign key (person_id) references Person(id)
+		on delete cascade on update cascade
+)
