97c146e050b4e5dbb1fd384f3a6b1dc9003161dc
Konstantinos Kougios <kostas.kougios@googlemail.com>
2012-08-07 21:39:54 +0100

    types suite, postgres

---
+++ src/test/resources/sql/TypesSuite.postgresql.sql
@@ -0,0 +1,24 @@
+[bd]
+create table BD (
+	id int not null,
+	big numeric(40,10),
+	bool boolean,
+	nv varchar(50),
+	tx text,
+	primary key (id)
+)
+
+[obd]
+create table OBD (
+	id int not null,
+	big numeric(40,10),
+	bool boolean,
+	nv varchar(50),
+	bt smallint,
+	small smallint,
+	int int,
+	long bigint,
+	float float,
+	double numeric(20,10),
+	primary key (id)
+)
