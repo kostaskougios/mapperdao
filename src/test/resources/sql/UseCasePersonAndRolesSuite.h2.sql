3d7fee1b473f51550a98a92e266cddc66dc621ea
kostas.kougios <kostas.kougios@googlemail.com>
2012-08-13 20:49:28 +0100

    impl mapperdao.select for declarePrimaryKeys

--- src/test/resources/sql/UseCasePersonAndRolesSuite.h2.sql
+++ src/test/resources/sql/UseCasePersonAndRolesSuite.h2.sql
@@ -2,45 +2,45 @@
 
 CREATE TABLE person
 (
-  id character varying(40) NOT NULL,
-  firstname character varying(255),
-  lastname character varying(255),
+  id varchar(40) NOT NULL,
+  firstname varchar(255),
+  lastname varchar(255),
   CONSTRAINT pk_party PRIMARY KEY (id )
 )
 ;
 CREATE TABLE roletype
 (
-  name character varying(40) NOT NULL,
-  description character varying(255),
+  name varchar(40) NOT NULL,
+  description varchar(255),
   CONSTRAINT pk_roleypte PRIMARY KEY (name )
 )
 ;
 CREATE TABLE singlepartyrole
 (
-  person_id character varying(40) NOT NULL,
-  roletype_name character varying(40) NOT NULL,
-  fromDate timestamp with time zone,
-  toDate timestamp with time zone,
-  CONSTRAINT pk_spr PRIMARY KEY (person_id , roletype_name ),
+  person_id varchar(40) NOT NULL,
+  roletype_name varchar(40) NOT NULL,
+  fromDate timestamp,
+  toDate timestamp,
+  CONSTRAINT pk_spr PRIMARY KEY (person_id , roletype_name),
   CONSTRAINT fk_spr_person FOREIGN KEY (person_id)
-      REFERENCES person (id) MATCH SIMPLE
+      REFERENCES person (id)
       ON UPDATE NO ACTION ON DELETE CASCADE,
   CONSTRAINT fk_to_role FOREIGN KEY (roletype_name)
-      REFERENCES roletype (name) MATCH SIMPLE
+      REFERENCES roletype (name)
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 ;
 CREATE TABLE interpartyrelationship
 (
-  from_id character varying(40) NOT NULL,
-  to_id character varying(40) NOT NULL,
-  fromdate timestamp with time zone,
-  todate timestamp with time zone,
-  CONSTRAINT pk_ipr PRIMARY KEY (from_id , to_id ),
+  from_id varchar(40) NOT NULL,
+  to_id varchar(40) NOT NULL,
+  fromdate timestamp,
+  todate timestamp,
+  CONSTRAINT pk_ipr PRIMARY KEY (from_id , to_id),
   CONSTRAINT fk_ipr_from FOREIGN KEY (from_id)
-      REFERENCES person (id) MATCH SIMPLE
+      REFERENCES person (id) 
       ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT fk_ipr_to FOREIGN KEY (to_id)
-      REFERENCES person (id) MATCH SIMPLE
+      REFERENCES person (id)
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
