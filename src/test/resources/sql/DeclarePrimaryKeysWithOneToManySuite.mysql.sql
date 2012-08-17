e2657f7d2774f9439d1b737945e8bf5e5460d9dd
Konstantinos Kougios <kostas.kougios@googlemail.com>
2012-08-17 18:29:32 +0100

    DeclarePrimaryKeysWithManyToOneSuite, fixed postgresql & mysql ddl

--- src/test/resources/sql/DeclarePrimaryKeysWithOneToManySuite.mysql.sql
+++ src/test/resources/sql/DeclarePrimaryKeysWithOneToManySuite.mysql.sql
@@ -3,7 +3,7 @@ create table Person (
 	email varchar(50) not null,
 	name varchar(20) not null,
 	primary key (email)
-)
+)  engine InnoDB
 ;
 create table LinkedPeople (
 	from_id varchar(50) not null,
@@ -12,4 +12,4 @@ create table LinkedPeople (
 	primary key (from_id,to_id),
 	constraint FK_LinkedPeople_From foreign key (from_id) references Person(email) on delete cascade,
 	constraint FK_LinkedPeople_To foreign key (to_id) references Person(email) on delete cascade
-)
+)  engine InnoDB
