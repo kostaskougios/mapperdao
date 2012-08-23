c1f4c0f251bd6d14891f7c44986798d3c5726f59
kostas.kougios <kostas.kougios@googlemail.com>
2012-08-23 20:27:32 +0100

    LowLevelQuerySuite to run only for h2

--- src/test/java/com/googlecode/mapperdao/LowLevelQuerySuite.scala
+++ src/test/java/com/googlecode/mapperdao/LowLevelQuerySuite.scala
@@ -15,34 +15,36 @@ import com.googlecode.mapperdao.jdbc.Setup
 class LowLevelQuerySuite extends FunSuite with ShouldMatchers {
 	import CommonEntities._
 
-	val (jdbc, mapperDao, queryDao) = Setup.setupMapperDao(TypeRegistry(ProductEntity, AttributeEntity))
+	if (Setup.database == "h2") {
+		val (jdbc, mapperDao, queryDao) = Setup.setupMapperDao(TypeRegistry(ProductEntity, AttributeEntity))
 
-	test("low level query, single entity") {
-		createProductAttribute(jdbc)
-		val red = mapperDao.insert(AttributeEntity, Attribute("colour", "red"))
-		val blue = mapperDao.insert(AttributeEntity, Attribute("colour", "blue"))
-		val acer = mapperDao.insert(AttributeEntity, Attribute("brand", "acer"))
+		test("low level query, single entity") {
+			createProductAttribute(jdbc)
+			val red = mapperDao.insert(AttributeEntity, Attribute("colour", "red"))
+			val blue = mapperDao.insert(AttributeEntity, Attribute("colour", "blue"))
+			val acer = mapperDao.insert(AttributeEntity, Attribute("brand", "acer"))
 
-		queryDao.lowLevelQuery(AttributeEntity, "select * from attribute where name=?", List("colour")).toSet should be === Set(red, blue)
-	}
+			queryDao.lowLevelQuery(AttributeEntity, "select * from attribute where name=?", List("colour")).toSet should be === Set(red, blue)
+		}
 
-	test("low level query, parent entities") {
-		createProductAttribute(jdbc)
-		val red = mapperDao.insert(AttributeEntity, Attribute("colour", "red"))
-		val blue = mapperDao.insert(AttributeEntity, Attribute("colour", "blue"))
-		val acer = mapperDao.insert(AttributeEntity, Attribute("brand", "acer"))
+		test("low level query, parent entities") {
+			createProductAttribute(jdbc)
+			val red = mapperDao.insert(AttributeEntity, Attribute("colour", "red"))
+			val blue = mapperDao.insert(AttributeEntity, Attribute("colour", "blue"))
+			val acer = mapperDao.insert(AttributeEntity, Attribute("brand", "acer"))
 
-		val p1 = mapperDao.insert(ProductEntity, Product("test1", Set(red)))
-		val p2 = mapperDao.insert(ProductEntity, Product("test2", Set(red, blue)))
-		val p3 = mapperDao.insert(ProductEntity, Product("test3", Set(acer, blue)))
+			val p1 = mapperDao.insert(ProductEntity, Product("test1", Set(red)))
+			val p2 = mapperDao.insert(ProductEntity, Product("test2", Set(red, blue)))
+			val p3 = mapperDao.insert(ProductEntity, Product("test3", Set(acer, blue)))
 
-		queryDao.lowLevelQuery(ProductEntity, """
+			queryDao.lowLevelQuery(ProductEntity, """
 				select p.* 
 				from product p 
 				inner join product_attribute pa on pa.product_id=p.id 
 				inner join attribute a on pa.attribute_id = a.id
 				where a.value=?
 				""", List("blue")).toSet should be === Set(p2, p3)
+		}
 	}
 }
 
