b6ea3b08e76b28dc9cacba136e2e1c25f44435d2
Konstantinos Kougios <kostas.kougios@googlemail.com>
2012-09-21 20:13:44 +0100

    made project compatible with scala mvn plugin

---
+++ src/test/scala/com/googlecode/mapperdao/InsertOrUpdateSuite.scala
@@ -0,0 +1,18 @@
+package com.googlecode.mapperdao
+
+import org.junit.runner.RunWith
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * @author kostantinos.kougios
+ *
+ * 21 Sep 2012
+ */
+@RunWith(classOf[JUnitRunner])
+class InsertOrUpdateSuite extends FunSuite with ShouldMatchers {
+	test("") {
+		fail
+	}
+}
