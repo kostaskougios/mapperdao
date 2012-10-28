d75350c9c58b394d58fec1e745021bc00fbc995a
kostas.kougios <kostas.kougios@googlemail.com>
2012-10-28 23:26:54 +0000

    DeleteSuite runs for all dbs

--- src/test/resources/sql/CommonEntities.postgresql.sql
+++ src/test/resources/sql/CommonEntities.postgresql.sql
@@ -1,3 +1,26 @@
+[product-attribute]
+create table Product (
+	id serial not null,
+	name varchar(100) not null,
+	primary key (id)
+)
+;
+create table Attribute (
+	id serial not null,
+	name varchar(100) not null,
+	value varchar(100) not null,
+	primary key(id)
+)
+;
+
+create table Product_Attribute (
+	product_id int not null,
+	attribute_id int not null,
+	primary key (product_id,attribute_id),
+	foreign key (product_id) references Product(id) on delete cascade on update cascade,
+	foreign key (attribute_id) references Attribute(id) on delete cascade on update cascade
+)
+
 [person-company]
 create table Company(
 	id serial not null,
