0e7d04122a9334bb2da529d4f5a74448fba2451e
Konstantinos Kougios <kostas.kougios@googlemail.com>
2012-11-05 09:09:42 +0000

    Update DSL, many-to-one

--- src/test/scala/com/googlecode/mapperdao/UpdateSuite.scala
+++ src/test/scala/com/googlecode/mapperdao/UpdateSuite.scala
@@ -17,6 +17,24 @@ class UpdateSuite extends FunSuite with ShouldMatchers {
 
 	val (jdbc, mapperDao, queryDao) = Setup.setupMapperDao(TypeRegistry(ProductEntity, AttributeEntity))
 
+	test("update many-to-one") {
+		createPersonCompany(jdbc)
+		val c1 = mapperDao.insert(CompanyEntity, Company("c1"))
+		val c2 = mapperDao.insert(CompanyEntity, Company("c2"))
+		val p1 = mapperDao.insert(PersonEntity, Person("p1", c1))
+		val p2 = mapperDao.insert(PersonEntity, Person("p2", c1))
+
+		import Update._
+		val pe = PersonEntity
+		(
+			update(pe)
+			set pe.company === c2
+			where pe.name === "p1"
+		).run(queryDao).rowsAffected should be === 1
+		mapperDao.select(PersonEntity, p1.id).get.company should be(c2)
+		mapperDao.select(PersonEntity, p2.id).get.company should be(c1)
+	}
+
 	test("update all") {
 		createProductAttribute(jdbc)
 		val (p1, p2) = createTestData
