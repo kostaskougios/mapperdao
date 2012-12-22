7ff0d1271c0bde73bb45f2dea8a7a65c0d660f73
Konstantinos Kougios <kostas.kougios@googlemail.com>
2012-12-22 19:40:52 +0000

    recreate many to many

--- src/main/scala/com/googlecode/mapperdao/MapperDaoImpl.scala
+++ src/main/scala/com/googlecode/mapperdao/MapperDaoImpl.scala
@@ -20,11 +20,12 @@ import com.googlecode.mapperdao.state.prioritise.PriorityPhase
 /**
  * @author kostantinos.kougios
  *
- * 13 Jul 2011
+ *         13 Jul 2011
  */
 protected final class MapperDaoImpl(
-		val driver: Driver,
-		val typeManager: TypeManager) extends MapperDao {
+	val driver: Driver,
+	val typeManager: TypeManager
+) extends MapperDao {
 	private val typeRegistry = driver.typeRegistry
 	private val lazyLoadManager = new LazyLoadManager
 	private val mockFactory = new MockFactory(typeManager)
@@ -43,6 +44,7 @@ protected final class MapperDaoImpl(
 		new OneToOneReverseDeletePlugin(typeRegistry, driver, this),
 		new ManyToOneDeletePlugin
 	)
+
 	/**
 	 * ===================================================================================
 	 * Utility methods
@@ -60,36 +62,39 @@ protected final class MapperDaoImpl(
 	override def insert[ID, PC <: DeclaredIds[ID], T](
 		updateConfig: UpdateConfig,
 		entity: Entity[ID, PC, T],
-		os: List[T]): List[T with PC] =
-		{
-			val po = new CmdPhase(typeManager)
-			val cmds = os.map { o =>
+		os: List[T]
+	): List[T with PC] = {
+		val po = new CmdPhase(typeManager)
+		val cmds = os.map {
+			o =>
 				if (isPersisted(o)) throw new IllegalArgumentException("can't insert an object that is already persisted: " + o)
 				val newVM = ValuesMap.fromEntity(typeManager, entity, o)
 				po.toInsertCmd(entity, newVM)
-			}.flatten
-			val pf = new PriorityPhase
-			val pri = pf.prioritise(entity, cmds)
+		}.flatten
+		val pf = new PriorityPhase
+		val pri = pf.prioritise(entity, cmds)
 
-			val ctd = new CmdToDatabase(updateConfig, driver, typeManager)
-			val nodes = ctd.execute(pri)
-			val recreationPhase = new RecreationPhase(updateConfig, mockFactory, typeManager, new UpdateEntityMap, nodes)
-			val recreated=recreationPhase.execute.asInstanceOf[List[T with PC]]
-      recreated
-		}
+		val ctd = new CmdToDatabase(updateConfig, driver, typeManager)
+		val nodes = ctd.execute(pri)
+		val recreationPhase = new RecreationPhase(updateConfig, mockFactory, typeManager, new UpdateEntityMap, nodes)
+		val recreated = recreationPhase.execute.asInstanceOf[List[T with PC]]
+		recreated
+	}
 
 	override def updateMutable[ID, PC <: DeclaredIds[ID], T](
 		updateConfig: UpdateConfig,
 		entity: Entity[ID, PC, T],
-		os: List[T with PC]): List[T with PC] = {
-		val osAndNewValues = os.map { o =>
-			o match {
-				case p: Persisted if (p.mapperDaoMock) =>
-					throw new IllegalStateException("Object %s is mock.".format(p))
-				case persisted: Persisted =>
-					val newValuesMap = ValuesMap.fromEntity(typeManager, entity, o)
-					(o, newValuesMap)
-			}
+		os: List[T with PC]
+	): List[T with PC] = {
+		val osAndNewValues = os.map {
+			o =>
+				o match {
+					case p: Persisted if (p.mapperDaoMock) =>
+						throw new IllegalStateException("Object %s is mock.".format(p))
+					case persisted: Persisted =>
+						val newValuesMap = ValuesMap.fromEntity(typeManager, entity, o)
+						(o, newValuesMap)
+				}
 		}
 		updateProcess(updateConfig, entity, osAndNewValues)
 	}
@@ -97,7 +102,8 @@ protected final class MapperDaoImpl(
 	override def updateImmutable[ID, PC <: DeclaredIds[ID], T](
 		updateConfig: UpdateConfig,
 		entity: Entity[ID, PC, T],
-		os: List[(T with PC, T)]): List[T with PC] = {
+		os: List[(T with PC, T)]
+	): List[T with PC] = {
 		val osAndNewValues = os.map {
 			case (oldO, newO) =>
 				oldO.mapperDaoDiscarded = true
@@ -110,7 +116,8 @@ protected final class MapperDaoImpl(
 	private def updateProcess[ID, PC <: DeclaredIds[ID], T](
 		updateConfig: UpdateConfig,
 		entity: Entity[ID, PC, T],
-		os: List[(T with PC, ValuesMap)]): List[T with PC] = {
+		os: List[(T with PC, ValuesMap)]
+	): List[T with PC] = {
 
 		val po = new CmdPhase(typeManager)
 		val cmds = os.map {
@@ -141,159 +148,165 @@ protected final class MapperDaoImpl(
 	 *
 	 * SelectConfig(skip=Set(ProductEntity.attributes)) // attributes won't be loaded
 	 */
-	override def select[ID, PC <: DeclaredIds[ID], T](selectConfig: SelectConfig, entity: Entity[ID, PC, T], id: ID) =
-		{
-			if (id == null) throw new NullPointerException("ids can't be null")
-			val ids = Helpers.idToList(id)
-			val pkSz = entity.tpe.table.primaryKeysSize
-			if (pkSz != ids.size) throw new IllegalArgumentException("entity has %d keys, can't use these keys: %s".format(pkSz, ids))
-			val entityMap = new EntityMap
-			val v = selectInner(entity, selectConfig, ids, entityMap)
-			v
-		}
+	override def select[ID, PC <: DeclaredIds[ID], T](selectConfig: SelectConfig, entity: Entity[ID, PC, T], id: ID) = {
+		if (id == null) throw new NullPointerException("ids can't be null")
+		val ids = Helpers.idToList(id)
+		val pkSz = entity.tpe.table.primaryKeysSize
+		if (pkSz != ids.size) throw new IllegalArgumentException("entity has %d keys, can't use these keys: %s".format(pkSz, ids))
+		val entityMap = new EntityMap
+		val v = selectInner(entity, selectConfig, ids, entityMap)
+		v
+	}
 
 	private[mapperdao] def selectInner[ID, PC <: DeclaredIds[ID], T](
 		entity: Entity[ID, PC, T],
 		selectConfig: SelectConfig,
 		ids: List[Any],
-		entities: EntityMap): Option[T with PC] =
-		{
-			val clz = entity.clz
-			val tpe = entity.tpe
-			if (tpe.table.primaryKeysSize != ids.size) throw new IllegalStateException("Primary keys number dont match the number of parameters. Primary keys: %s".format(tpe.table.primaryKeys))
+		entities: EntityMap
+	): Option[T with PC] = {
+		val clz = entity.clz
+		val tpe = entity.tpe
+		if (tpe.table.primaryKeysSize != ids.size) throw new IllegalStateException("Primary keys number dont match the number of parameters. Primary keys: %s".format(tpe.table.primaryKeys))
 
-			entities.get[T with PC](tpe.clz, ids) {
-				try {
-					val (pks, declared) = ids.splitAt(tpe.table.primaryKeys.size)
-					val pkArgs = tpe.table.primaryKeys.zip(pks)
-					// convert unused keys to their simple values
-					val declaredArgs = if (tpe.table.unusedPKs.isEmpty)
-						Nil
-					else
-						(
-							(tpe.table.unusedPKColumnInfos zip declared) map {
-								case (ci, v) =>
-									ci match {
-										case ci: ColumnInfoManyToOne[T, Any, DeclaredIds[Any], Any] =>
-											val foreign = ci.column.foreign
-											val fentity = foreign.entity
-											val ftable = fentity.tpe.table
-											ci.column.columns zip ftable.toListOfPrimaryKeyValues(v)
-										case ci: ColumnInfoTraversableOneToMany[Any, DeclaredIds[Any], Any, Any, DeclaredIds[Any], Any] =>
-											val fentity = ci.entityOfT
-											val ftable = fentity.tpe.table
-											ci.column.columns zip ftable.toListOfPrimaryKeyValues(v)
-										case ci: ColumnInfoOneToOne[T, Any, DeclaredIds[Any], Any] =>
-											val foreign = ci.column.foreign
-											val fentity = foreign.entity
-											val ftable = fentity.tpe.table
-											ci.column.columns zip ftable.toListOfPrimaryKeyValues(v)
-										case _ => throw new IllegalArgumentException("Please use declarePrimaryKey only for relationships. For normal data please use key(). This occured for entity %s".format(entity.getClass))
-									}
-							}).flatten
+		entities.get[T with PC](tpe.clz, ids) {
+			try {
+				val (pks, declared) = ids.splitAt(tpe.table.primaryKeys.size)
+				val pkArgs = tpe.table.primaryKeys.zip(pks)
+				// convert unused keys to their simple values
+				val declaredArgs = if (tpe.table.unusedPKs.isEmpty)
+					Nil
+				else
+					(
+						(tpe.table.unusedPKColumnInfos zip declared) map {
+							case (ci, v) =>
+								ci match {
+									case ci: ColumnInfoManyToOne[T, Any, DeclaredIds[Any], Any] =>
+										val foreign = ci.column.foreign
+										val fentity = foreign.entity
+										val ftable = fentity.tpe.table
+										ci.column.columns zip ftable.toListOfPrimaryKeyValues(v)
+									case ci: ColumnInfoTraversableOneToMany[Any, DeclaredIds[Any], Any, Any, DeclaredIds[Any], Any] =>
+										val fentity = ci.entityOfT
+										val ftable = fentity.tpe.table
+										ci.column.columns zip ftable.toListOfPrimaryKeyValues(v)
+									case ci: ColumnInfoOneToOne[T, Any, DeclaredIds[Any], Any] =>
+										val foreign = ci.column.foreign
+										val fentity = foreign.entity
+										val ftable = fentity.tpe.table
+										ci.column.columns zip ftable.toListOfPrimaryKeyValues(v)
+									case _ => throw new IllegalArgumentException("Please use declarePrimaryKey only for relationships. For normal data please use key(). This occured for entity %s".format(entity.getClass))
+								}
+						}).flatten
 
-					val args = pkArgs ::: declaredArgs
+				val args = pkArgs ::: declaredArgs
 
-					val om = driver.doSelect(selectConfig, tpe, args)
-					if (om.isEmpty) None
-					else if (om.size > 1) throw new IllegalStateException("expected 1 result for %s and ids %s, but got %d. Is the primary key column a primary key in the table?".format(clz.getSimpleName, ids, om.size))
-					else {
-						val l = toEntities(om, entity, selectConfig, entities)
-						Some(l.head)
-					}
-				} catch {
-					case e => throw new QueryException("An error occured during select of entity %s and primary keys %s".format(entity, ids), e)
+				val om = driver.doSelect(selectConfig, tpe, args)
+				if (om.isEmpty) None
+				else if (om.size > 1) throw new IllegalStateException("expected 1 result for %s and ids %s, but got %d. Is the primary key column a primary key in the table?".format(clz.getSimpleName, ids, om.size))
+				else {
+					val l = toEntities(om, entity, selectConfig, entities)
+					Some(l.head)
 				}
+			} catch {
+				case e => throw new QueryException("An error occured during select of entity %s and primary keys %s".format(entity, ids), e)
 			}
 		}
+	}
 
 	private[mapperdao] def toEntities[ID, PC <: DeclaredIds[ID], T](
 		lm: List[DatabaseValues],
 		entity: Entity[ID, PC, T],
 		selectConfig: SelectConfig,
-		entities: EntityMap): List[T with PC] =
-		lm.map { jdbcMap =>
-			val tpe = entity.tpe
-			val table = tpe.table
-			// calculate the id's for this tpe
-			val pkIds = table.primaryKeys.map { pk => jdbcMap(pk) } ::: selectBeforePlugins.map {
-				_.idContribution(tpe, jdbcMap, entities)
-			}.flatten
-			val unusedIds = table.unusedPKs.map { pk =>
-				jdbcMap(pk)
-			}
-			val ids = pkIds ::: unusedIds
-			if (ids.isEmpty)
-				throw new IllegalStateException("entity %s without primary key, please use declarePrimaryKeys() to declare the primary key columns of tables into your entity declaration")
+		entities: EntityMap
+	): List[T with PC] =
+		lm.map {
+			jdbcMap =>
+				val tpe = entity.tpe
+				val table = tpe.table
+				// calculate the id's for this tpe
+				val pkIds = table.primaryKeys.map { pk => jdbcMap(pk) } ::: selectBeforePlugins.map {
+					_.idContribution(tpe, jdbcMap, entities)
+				}.flatten
+				val unusedIds = table.unusedPKs.map {
+					pk =>
+						jdbcMap(pk)
+				}
+				val ids = pkIds ::: unusedIds
+				if (ids.isEmpty)
+					throw new IllegalStateException("entity %s without primary key, please use declarePrimaryKeys() to declare the primary key columns of tables into your entity declaration")
 
-			entities.get[T with PC](tpe.clz, ids) {
-				val mods = jdbcMap.toMap
-				val mock = mockFactory.createMock(selectConfig.data, entity, mods)
-				entities.putMock(tpe.clz, ids, mock)
+				entities.get[T with PC](tpe.clz, ids) {
+					val mods = jdbcMap.toMap
+					val mock = mockFactory.createMock(selectConfig.data, entity, mods)
+					entities.putMock(tpe.clz, ids, mock)
 
-				val allMods = mods ++ selectBeforePlugins.map {
-					_.before(entity, selectConfig, jdbcMap, entities)
-				}.flatten.map {
-					case SelectMod(k, v, lazyBeforeLoadVal) =>
-						(k, v)
-				}.toMap
+					val allMods = mods ++ selectBeforePlugins.map {
+						_.before(entity, selectConfig, jdbcMap, entities)
+					}.flatten.map {
+						case SelectMod(k, v, lazyBeforeLoadVal) =>
+							(k, v)
+					}.toMap
 
-				val vm = ValuesMap.fromMap(-1, allMods)
-				// if the entity should be lazy loaded and it has relationships, then
-				// we need to lazy load it
-				val entityV = if (lazyLoadManager.isLazyLoaded(selectConfig.lazyLoad, entity)) {
-					lazyLoadEntity(entity, selectConfig, vm)
-				} else tpe.constructor(selectConfig.data, vm)
-				Some(entityV)
-			}.get
+					val vm = ValuesMap.fromMap(-1, allMods)
+					// if the entity should be lazy loaded and it has relationships, then
+					// we need to lazy load it
+					val entityV = if (lazyLoadManager.isLazyLoaded(selectConfig.lazyLoad, entity)) {
+						lazyLoadEntity(entity, selectConfig, vm)
+					} else tpe.constructor(selectConfig.data, vm)
+					Some(entityV)
+				}.get
 		}
 
 	private def lazyLoadEntity[ID, PC <: DeclaredIds[ID], T](
 		entity: Entity[ID, PC, T],
 		selectConfig: SelectConfig,
-		vm: ValuesMap) = {
+		vm: ValuesMap
+	) = {
 		// substitute lazy loaded columns with empty values
 		val tpe = entity.tpe
 		val table = tpe.table
 		val lazyLoad = selectConfig.lazyLoad
 
-		val lazyLoadedMods = (table.columnInfosPlain.map { ci =>
-			val ll = lazyLoad.isLazyLoaded(ci)
-			ci match {
-				case mtm: ColumnInfoTraversableManyToMany[_, _, _, _] =>
-					(ci.column.alias, if (ll) Nil else vm.valueOf(ci))
-				case mto: ColumnInfoManyToOne[_, _, _, _] =>
-					(ci.column.alias, if (ll) null else vm.valueOf(ci))
-				case mtm: ColumnInfoTraversableOneToMany[_, _, _, _, _, _] =>
-					(ci.column.alias, if (ll) Nil else vm.valueOf(ci))
-				case otor: ColumnInfoOneToOneReverse[_, _, _, _] =>
-					(ci.column.alias, if (ll) null else vm.valueOf(ci))
-				case _ => (ci.column.alias, vm.valueOf(ci))
-			}
-		} ::: table.extraColumnInfosPersisted.map { ci =>
-			(ci.column.alias, vm.valueOf(ci))
+		val lazyLoadedMods = (table.columnInfosPlain.map {
+			ci =>
+				val ll = lazyLoad.isLazyLoaded(ci)
+				ci match {
+					case mtm: ColumnInfoTraversableManyToMany[_, _, _, _] =>
+						(ci.column.alias, if (ll) Nil else vm.valueOf(ci))
+					case mto: ColumnInfoManyToOne[_, _, _, _] =>
+						(ci.column.alias, if (ll) null else vm.valueOf(ci))
+					case mtm: ColumnInfoTraversableOneToMany[_, _, _, _, _, _] =>
+						(ci.column.alias, if (ll) Nil else vm.valueOf(ci))
+					case otor: ColumnInfoOneToOneReverse[_, _, _, _] =>
+						(ci.column.alias, if (ll) null else vm.valueOf(ci))
+					case _ => (ci.column.alias, vm.valueOf(ci))
+				}
+		} ::: table.extraColumnInfosPersisted.map {
+			ci =>
+				(ci.column.alias, vm.valueOf(ci))
 		}).toMap
 		val lazyLoadedVM = ValuesMap.fromMap(vm.identity, lazyLoadedMods)
 		val constructed = tpe.constructor(selectConfig.data, lazyLoadedVM)
 		val proxy = lazyLoadManager.proxyFor(constructed, entity, lazyLoad, vm)
 		proxy
 	}
+
 	/**
 	 * create a mock of the current entity, to avoid cyclic dependencies
 	 * doing infinite loops.
 	 */
 
-	override def delete[ID, PC <: DeclaredIds[ID], T](entity: Entity[ID, PC, T], id: ID): Unit =
-		{
-			val ids = Helpers.idToList(id)
-			val tpe = entity.tpe
-			val table = tpe.table
-			val pks = table.primaryKeys
-			if (pks.size != ids.size) throw new IllegalArgumentException("number of primary key values don't match number of primary keys : %s != %s".format(pks, ids))
-			val keyValues = pks zip ids
-			// do the actual delete database op
-			driver.doDelete(tpe, keyValues)
-		}
+	override def delete[ID, PC <: DeclaredIds[ID], T](entity: Entity[ID, PC, T], id: ID): Unit = {
+		val ids = Helpers.idToList(id)
+		val tpe = entity.tpe
+		val table = tpe.table
+		val pks = table.primaryKeys
+		if (pks.size != ids.size) throw new IllegalArgumentException("number of primary key values don't match number of primary keys : %s != %s".format(pks, ids))
+		val keyValues = pks zip ids
+		// do the actual delete database op
+		driver.doDelete(tpe, keyValues)
+	}
+
 	/**
 	 * deletes an entity from the database
 	 */
@@ -308,7 +321,8 @@ protected final class MapperDaoImpl(
 		deleteConfig: DeleteConfig,
 		entity: Entity[ID, PC, T],
 		o: T with PC,
-		entityMap: UpdateEntityMap): T = {
+		entityMap: UpdateEntityMap
+	): T = {
 		if (o.mapperDaoDiscarded) throw new IllegalArgumentException("can't operate on an object twice. An object that was updated/deleted must be discarded and replaced by the return value of update(), i.e. onew=update(o) or just be disposed if it was deleted. The offending object was : " + o);
 
 		val tpe = entity.tpe
@@ -347,7 +361,8 @@ protected final class MapperDaoImpl(
 		updateConfig: UpdateConfig,
 		entity: Entity[ID, PC, T],
 		o: T,
-		ids: ID): T with PC =
+		ids: ID
+	): T with PC =
 		select(selectConfig, entity, ids) match {
 			case None => insert(updateConfig, entity, o)
 			case Some(oldO) =>
--- src/main/scala/com/googlecode/mapperdao/state/recreation/RecreationPhase.scala
+++ src/main/scala/com/googlecode/mapperdao/state/recreation/RecreationPhase.scala
@@ -8,43 +8,51 @@ import com.googlecode.mapperdao._
  *
  * @author kostantinos.kougios
  *
- * 11 Dec 2012
+ *         11 Dec 2012
  */
 class RecreationPhase(
-		updateConfig: UpdateConfig,
-		mockFactory: MockFactory,
-		typeManager: TypeManager,
-		entityMap: UpdateEntityMap,
-		nodes: List[PersistedNode[_, _]]) {
+	updateConfig: UpdateConfig,
+	mockFactory: MockFactory,
+	typeManager: TypeManager,
+	entityMap: UpdateEntityMap,
+	nodes: List[PersistedNode[_, _]]
+) {
 
-	private val byIdentity: Map[Int, PersistedNode[_, _]] = nodes.map { node =>
-		(node.newVM.identity, node)
+	private val byIdentity: Map[Int, PersistedNode[_, _]] = nodes.map {
+		node =>
+			(node.newVM.identity, node)
 	}.toMap
 
 	def execute = {
-		recreate(nodes)
+		recreate(nodes.filter(_.mainEntity))
 	}
 
 	private def recreate(nodes: List[PersistedNode[_, _]]) =
-		nodes.map { node =>
-			entityMap.get[DeclaredIds[Any], Any](node.identity).getOrElse {
-				val entity = node.entity
-				val tpe = entity.tpe
-				val table = tpe.table
+		nodes.map {
+			node =>
+				entityMap.get[DeclaredIds[Any], Any](node.identity).getOrElse {
+					val entity = node.entity
+					val tpe = entity.tpe
+					val table = tpe.table
 
-				val newVM = node.newVM
-				val modified = newVM.toMap
+					val newVM = node.newVM
+					val modified = newVM.toMap
 
-				// create a mock
-				var mockO = mockFactory.createMock(updateConfig.data, entity, modified)
-				entityMap.put(node.identity, mockO)
+					// create a mock
+					val mockO = mockFactory.createMock(updateConfig.data, entity, modified)
+					entityMap.put(node.identity, mockO)
 
-				val finalMods = modified
-				val newE = tpe.constructor(updateConfig.data, ValuesMap.fromMap(node.identity, finalMods))
-				// re-put the actual
-				entityMap.put(node.identity, newE)
-				newE.asInstanceOf[DeclaredIds[Any]]
-			}
+					table.relationshipColumnInfos.map {
+						case ColumnInfoTraversableManyToMany(column, columnToValue, getterMethod) =>
+							val relatedNodes=newVM.valueOf[Iterable[_]](column).map(System.identityHashCode(_)).map (byIdentity(_))
+							println(relatedNodes)
+					}
+
+					val finalMods = modified
+					val newE = tpe.constructor(updateConfig.data, ValuesMap.fromMap(node.identity, finalMods))
+					// re-put the actual
+					entityMap.put(node.identity, newE)
+					newE.asInstanceOf[DeclaredIds[Any]]
+				}
 		}
-
 }
