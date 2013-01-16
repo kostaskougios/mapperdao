35074b3e44d73007fd8a80cc3a59fe8b50506923
kostas.kougios <kostas.kougios@googlemail.com>
2013-01-16 22:53:07 +0000

    many-to-many re-impl

--- src/test/scala/com/googlecode/mapperdao/OneToManyWithoutFKQuerySuite.scala
+++ src/test/scala/com/googlecode/mapperdao/OneToManyWithoutFKQuerySuite.scala
@@ -1,4 +1,5 @@
 package com.googlecode.mapperdao
+
 import org.junit.runner.RunWith
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FunSuite
@@ -8,7 +9,7 @@ import org.scalatest.junit.JUnitRunner
 /**
  * @author kostantinos.kougios
  *
- * May 21, 2012
+ *         May 21, 2012
  */
 @RunWith(classOf[JUnitRunner])
 class OneToManyWithoutFKQuerySuite extends FunSuite with ShouldMatchers {
@@ -37,40 +38,47 @@ class OneToManyWithoutFKQuerySuite extends FunSuite with ShouldMatchers {
 	}
 
 	case class Product(val id: Int, var name: String, infos: Set[Info])
+
 	case class Info(val title: String, loc: Location)
+
 	case class Location(id: Int, name: String)
 
 	object LocationEntity extends ExternalEntity[Int, Location] {
 		val id = key("id") to (_.id)
 
-		onInsertManyToOne { i =>
-			PrimaryKeysValues(i.foreign.id)
+		onInsertManyToOne {
+			i =>
+				PrimaryKeysValues(i.foreign.id)
 		}
-		onSelectManyToOne { s =>
-			s.primaryKeys match {
-				case (id: Int) :: Nil => id match {
-					case 1 => l1
-					case 2 => l2
+		onSelectManyToOne {
+			s =>
+				s.primaryKeys match {
+					case (id: Int) :: Nil => id match {
+						case 1 => l1
+						case 2 => l2
+					}
+					case _ => throw new IllegalStateException("LocationEntity, shouldn't get here for " + s)
 				}
-				case _ => throw new IllegalStateException("LocationEntity, shouldn't get here for " + s)
-			}
 		}
 	}
 
-	object InfoEntity extends Entity[Location, With1Id[Location], Info] {
+	object InfoEntity extends Entity[Location, Info] {
+		type Stored = With1Id[Location]
 		val title = column("title") to (_.title)
 		val loc = manytoone(LocationEntity) to (_.loc)
 
 		declarePrimaryKey(loc)
 
-		def constructor(implicit m) = new Info(title, loc) with With1Id[Location]
+		def constructor(implicit m) = new Info(title, loc) with Stored
 	}
 
-	object ProductEntity extends Entity[Int, SurrogateIntId, Product] {
+	object ProductEntity extends Entity[Int, Product] {
+		type Stored = SurrogateIntId
 		val id = key("id") to (_.id)
 		val name = column("name") to (_.name)
 		val infos = onetomany(InfoEntity) to (_.infos)
 
-		def constructor(implicit m) = new Product(id, name, infos) with SurrogateIntId
+		def constructor(implicit m) = new Product(id, name, infos) with Stored
 	}
+
 }
