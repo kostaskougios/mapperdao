b580fd63660baabd3527a345d6f5342cbf1f255f
kostas.kougios@gmail.com <sb3Xk9Eq5Nm2>
2013-02-11 22:02:36 +0000

    batch tests

--- src/test/scala/com/googlecode/mapperdao/ManyToOneSuite.scala
+++ src/test/scala/com/googlecode/mapperdao/ManyToOneSuite.scala
@@ -15,7 +15,7 @@ import com.googlecode.mapperdao.utils.Helpers
 @RunWith(classOf[JUnitRunner])
 class ManyToOneSuite extends FunSuite with ShouldMatchers {
 
-	import ManyToOneSpec._
+	import ManyToOneSuite._
 
 	val (jdbc, mapperDao, queryDao) = Setup.setupMapperDao(TypeRegistry(PersonEntity, CompanyEntity, HouseEntity))
 
@@ -53,6 +53,81 @@ class ManyToOneSuite extends FunSuite with ShouldMatchers {
 		mapperDao.select(PersonEntity, i1.id).get should be(i1)
 		mapperDao.select(PersonEntity, i2.id).get should be(i2)
 		mapperDao.select(PersonEntity, i3.id).get should be(i3)
+	}
+
+	test("batch insert, existing one-part") {
+		createTables
+
+		val List(c1, c2) = mapperDao.insertBatch(CompanyEntity, List(Company(101, "C1"), Company(102, "C2")))
+
+		val h1 = House(201, "H1")
+		val h2 = House(202, "H2")
+
+		val p1 = Person(1, "P1", c1, h1)
+		val p2 = Person(2, "P2", c2, h2)
+		val p3 = Person(3, "P2", c2, h1)
+
+		val List(i1, i2, i3) = mapperDao.insertBatch(PersonEntity, List(p1, p2, p3))
+		i1 should be(p1)
+		i2 should be(p2)
+		i3 should be(p3)
+
+		mapperDao.select(PersonEntity, i1.id).get should be(i1)
+		mapperDao.select(PersonEntity, i2.id).get should be(i2)
+		mapperDao.select(PersonEntity, i3.id).get should be(i3)
+	}
+
+	test("batch update on inserted") {
+		createTables
+
+		val List(c1, c2) = mapperDao.insertBatch(CompanyEntity, List(Company(101, "C1"), Company(102, "C2")))
+		val List(h1, h2) = mapperDao.insertBatch(HouseEntity, List(House(201, "H1"), House(202, "H2")))
+
+		val p1 = Person(1, "P1", c1, h1)
+		val p2 = Person(2, "P2", c2, h2)
+		val p3 = Person(3, "P2", c2, h1)
+
+		val List(i1, i2, i3) = mapperDao.insertBatch(PersonEntity, List(p1, p2, p3))
+
+		val u1 = i1.copy(lives = h2, company = c2)
+		val u2 = i2.copy(lives = h1, company = c1)
+		val u3 = i3.copy(lives = h2, company = c1)
+
+		val List(up1, up2, up3) = mapperDao.updateBatch(PersonEntity, List((i1, u1), (i2, u2), (i3, u3)))
+		up1 should be(u1)
+		up2 should be(u2)
+		up3 should be(u3)
+		mapperDao.select(PersonEntity, up1.id).get should be(u1)
+		mapperDao.select(PersonEntity, up2.id).get should be(u2)
+		mapperDao.select(PersonEntity, up3.id).get should be(u3)
+	}
+
+	test("batch update on selected") {
+		createTables
+
+		val List(c1, c2) = mapperDao.insertBatch(CompanyEntity, List(Company(101, "C1"), Company(102, "C2")))
+		val List(h1, h2) = mapperDao.insertBatch(HouseEntity, List(House(201, "H1"), House(202, "H2")))
+
+		val p1 = Person(1, "P1", c1, h1)
+		val p2 = Person(2, "P2", c2, h2)
+		val p3 = Person(3, "P2", c2, h1)
+
+		val List(i1, i2, i3) = mapperDao.insertBatch(PersonEntity, List(p1, p2, p3)).map {
+			p =>
+				mapperDao.select(PersonEntity, p.id).get
+		}
+
+		val u1 = i1.copy(lives = h2, company = c2)
+		val u2 = i2.copy(lives = h1, company = c1)
+		val u3 = i3.copy(lives = h2, company = c1)
+
+		val List(up1, up2, up3) = mapperDao.updateBatch(PersonEntity, List((i1, u1), (i2, u2), (i3, u3)))
+		up1 should be(u1)
+		up2 should be(u2)
+		up3 should be(u3)
+		mapperDao.select(PersonEntity, up1.id).get should be(u1)
+		mapperDao.select(PersonEntity, up2.id).get should be(u2)
+		mapperDao.select(PersonEntity, up3.id).get should be(u3)
 	}
 
 	test("insert") {
@@ -193,7 +268,7 @@ class ManyToOneSuite extends FunSuite with ShouldMatchers {
 	}
 }
 
-object ManyToOneSpec {
+object ManyToOneSuite {
 
 	case class Person(val id: Int, val name: String, val company: Company, val lives: House)
 
