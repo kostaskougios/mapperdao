e109a4375b7b60e529367a0a031abee4f15f8619
kostas.kougios@gmail.com <sb3Xk9Eq5Nm2>
2013-02-26 21:56:14 +0000

    fixing tests

--- src/test/scala/com/googlecode/mapperdao/OneToOneReverseCompositeKeySuite.scala
+++ src/test/scala/com/googlecode/mapperdao/OneToOneReverseCompositeKeySuite.scala
@@ -10,12 +10,14 @@ import com.googlecode.mapperdao.utils.Helpers
 /**
  * @author kostantinos.kougios
  *
- * 30 Jul 2012
+ *         30 Jul 2012
  */
 @RunWith(classOf[JUnitRunner])
-class OneToOneReverseCompositeKeySuite extends FunSuite with ShouldMatchers {
+class OneToOneReverseCompositeKeySuite extends FunSuite with ShouldMatchers
+{
 
 	import OneToOneReverseCompositeKeySuite._
+
 	if (database != "h2") {
 		implicit val (jdbc, mapperDao, queryDao) = Setup.setupMapperDao(TypeRegistry(InventoryEntity, ProductEntity))
 		var noiseId = 100
@@ -29,49 +31,49 @@ class OneToOneReverseCompositeKeySuite extends FunSuite with ShouldMatchers {
 			noise
 			noise
 			val inserted1 = mapperDao.insert(InventoryEntity, Inventory(100, "rc1", Product("product 1", null), 5))
-			val inserted1a = mapperDao.insert(InventoryEntity, Inventory(101, "rc1", Product("product 1a", null), 5))
+			mapperDao.insert(InventoryEntity, Inventory(101, "rc1", Product("product 1a", null), 5))
 			val inserted2 = mapperDao.insert(InventoryEntity, Inventory(100, "rc2", Product("product 2", null), 6))
-			val inserted2a = mapperDao.insert(InventoryEntity, Inventory(101, "rc2", Product("product 2a", null), 6))
+			mapperDao.insert(InventoryEntity, Inventory(101, "rc2", Product("product 2a", null), 6))
 
 			import Query._
 			(
 				select
-				from pe
-				join (pe, pe.inventory, ie)
-				where pe.inventory === inserted2
-			).toSet should be === Set(Product("product 2", Inventory(100, "rc2", Product("product 2", null), 6)))
+					from pe
+					join(pe, pe.inventory, ie)
+					where pe.inventory === inserted2
+				).toSet should be === Set(Product("product 2", Inventory(100, "rc2", Product("product 2", null), 6)))
 
 			(
 				select
-				from pe
-				join (pe, pe.inventory, ie)
-				where pe.inventory === inserted1
-			).toSet should be === Set(Product("product 1", Inventory(100, "rc1", Product("product 1", null), 5)))
+					from pe
+					join(pe, pe.inventory, ie)
+					where pe.inventory === inserted1
+				).toSet should be === Set(Product("product 1", Inventory(100, "rc1", Product("product 1", null), 5)))
 
 			(
 				select
-				from pe
-				join (pe, pe.inventory, ie)
-				where ie.refCode === "rc1"
-			).toSet should be === Set(
-					Product("product 1", Inventory(100, "rc1", Product("product 1", null), 5)),
-					Product("product 1a", Inventory(101, "rc1", Product("product 1a", null), 5))
-				)
+					from pe
+					join(pe, pe.inventory, ie)
+					where ie.refCode === "rc1"
+				).toSet should be === Set(
+				Product("product 1", Inventory(100, "rc1", Product("product 1", null), 5)),
+				Product("product 1a", Inventory(101, "rc1", Product("product 1a", null), 5))
+			)
 		}
 
 		test("query") {
 			createTables()
 			noise
 			noise
-			val inserted1 = mapperDao.insert(InventoryEntity, Inventory(1, "rc1", Product("product 1", null), 5))
-			val inserted2 = mapperDao.insert(InventoryEntity, Inventory(2, "rc2", Product("product 2", null), 6))
+			mapperDao.insert(InventoryEntity, Inventory(1, "rc1", Product("product 1", null), 5))
+			mapperDao.insert(InventoryEntity, Inventory(2, "rc2", Product("product 2", null), 6))
 
 			import Query._
 			(
 				select
-				from ie
-				where ie.stock === 6
-			).toSet should be === Set(Inventory(2, "rc2", Product("product 2", Inventory(2, "rc2", Product("product 2", null), 6)), 6))
+					from ie
+					where ie.stock === 6
+				).toSet should be === Set(Inventory(2, "rc2", Product("product 2", Inventory(2, "rc2", Product("product 2", null), 6)), 6))
 		}
 
 		test("create, select and delete") {
@@ -121,41 +123,48 @@ class OneToOneReverseCompositeKeySuite extends FunSuite with ShouldMatchers {
 			mapperDao.insert(InventoryEntity, Inventory(noiseId, "noise-ref", Product("a nice & noisy product " + noiseId, null), 8))
 		}
 
-		def createTables() =
-			{
-				Setup.dropAllTables(jdbc)
-				Setup.queries(this, jdbc).update("ddl")
-				if (Setup.database == "oracle") {
-					Setup.createSeq(jdbc, "ProductSeq")
-				}
+		def createTables() = {
+			Setup.dropAllTables(jdbc)
+			Setup.queries(this, jdbc).update("ddl")
+			if (Setup.database == "oracle") {
+				Setup.createSeq(jdbc, "ProductSeq")
 			}
+		}
 	}
 }
 
-object OneToOneReverseCompositeKeySuite {
+object OneToOneReverseCompositeKeySuite
+{
 	val database = Setup.database
 
 	case class Inventory(id: Int, refCode: String, product: Product, stock: Int)
+
 	case class Product(name: String, inventory: Inventory)
 
-	object InventoryEntity extends Entity[(Int, String), NaturalIntAndNaturalStringIds, Inventory] {
+	object InventoryEntity extends Entity[(Int, String), Inventory]
+	{
+		type Stored = NaturalIntAndNaturalStringIds
 		val id = key("id") to (_.id)
 		val refCode = key("refCode") to (_.refCode)
 		val product = onetoone(ProductEntity) to (_.product)
 		val stock = column("stock") to (_.stock)
 
-		def constructor(implicit m) = new Inventory(id, refCode, product, stock) with NaturalIntAndNaturalStringIds
+		def constructor(implicit m) = new Inventory(id, refCode, product, stock) with Stored
 	}
 
-	object ProductEntity extends Entity[Int, SurrogateIntId, Product] {
+	object ProductEntity extends Entity[Int, Product]
+	{
+		type Stored = SurrogateIntId
 		val id = key("id") sequence (
 			if (database == "oracle") Some("ProductSeq") else None
-		) autogenerated (_.id)
+			) autogenerated (_.id)
 		val name = column("name") to (_.name)
 		val inventory = onetoonereverse(InventoryEntity) to (_.inventory)
 
-		def constructor(implicit m) = new Product(name, inventory) with SurrogateIntId {
+		def constructor(implicit m) = new Product(name, inventory) with Stored
+		{
 			val id: Int = ProductEntity.id
 		}
 	}
+
 }
