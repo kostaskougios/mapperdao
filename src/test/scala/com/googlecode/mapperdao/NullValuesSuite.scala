51f892b44799c08c81672cb4f31560c54a3350c4
kostas.kougios@gmail.com <sb3Xk9Eq5Nm2>
2013-03-01 23:18:11 +0000

    tests

--- src/test/scala/com/googlecode/mapperdao/NullValuesSuite.scala
+++ src/test/scala/com/googlecode/mapperdao/NullValuesSuite.scala
@@ -9,12 +9,13 @@ import com.googlecode.mapperdao.jdbc.Setup
 /**
  * @author kostantinos.kougios
  *
- * 19 May 2012
+ *         19 May 2012
  */
 @RunWith(classOf[JUnitRunner])
-class NullValuesSuite extends FunSuite with ShouldMatchers {
+class NullValuesSuite extends FunSuite with ShouldMatchers
+{
 	if (Setup.database == "h2") {
-		val (jdbc, mapperDao, queryDao) = Setup.setupMapperDao(TypeRegistry(ValuesEntity))
+		val (jdbc, mapperDao, _) = Setup.setupMapperDao(TypeRegistry(ValuesEntity))
 
 		val v = Values(1, null, null, null, null)
 
@@ -42,23 +43,25 @@ class NullValuesSuite extends FunSuite with ShouldMatchers {
 			mapperDao.select(ValuesEntity, 1).get.d should be(null)
 		}
 
-		def createTables =
-			{
-				Setup.dropAllTables(jdbc)
-				Setup.queries(this, jdbc).update("ddl")
-			}
+		def createTables = {
+			Setup.dropAllTables(jdbc)
+			Setup.queries(this, jdbc).update("ddl")
+		}
 
 	}
 
 	case class Values(id: Int, i: java.lang.Integer, l: java.lang.Long, f: java.lang.Float, d: java.lang.Double)
 
-	object ValuesEntity extends Entity[Int, SurrogateIntId, Values] {
+	object ValuesEntity extends Entity[Int, Values]
+	{
+		type Stored = SurrogateIntId
 		val id = key("id") to (_.id)
 		val i = column("i") to (_.i)
 		val l = column("l") to (_.l)
 		val f = column("f") to (_.f)
 		val d = column("d") to (_.d)
 
-		def constructor(implicit m) = new Values(id, i, l, f, d) with SurrogateIntId
+		def constructor(implicit m) = new Values(id, i, l, f, d) with Stored
 	}
+
 }
