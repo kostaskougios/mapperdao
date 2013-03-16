9e0cbd773c315b4531c3bf51c7568304570bcfb4
Konstantinos Kougios <kostas.kougios@googlemail.com>
2013-03-16 18:56:38 +0000

    are inserts/updates optimal?

--- src/test/scala/com/googlecode/mapperdao/jdbc/CmdToDatabaseSuite.scala
+++ src/test/scala/com/googlecode/mapperdao/jdbc/CmdToDatabaseSuite.scala
@@ -5,6 +5,7 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 import com.googlecode.mapperdao._
+import drivers.Driver
 import org.scalatest.mock.EasyMockSugar
 import state.persistcmds.CmdPhase
 import state.prioritise.PriorityPhase
@@ -17,8 +18,6 @@ import state.prioritise.PriorityPhase
 class CmdToDatabaseSuite extends FunSuite with ShouldMatchers with EasyMockSugar
 {
 
-	import OneToManyDeclarePrimaryKeysSuite._
-
 	def prepare(l: List[Entity[_, Persisted, _]]) = {
 		val typeManager = new DefaultTypeManager
 		val (jdbc, mapperDao: MapperDaoImpl, _) = Setup.setupMapperDao(TypeRegistry(l))
@@ -28,7 +27,49 @@ class CmdToDatabaseSuite extends FunSuite with ShouldMatchers with EasyMockSugar
 
 	val uc = UpdateConfig.default
 
+	test("optimized updates for many-to-one, nothing to update") {
+		import CommonEntities._
+		val (typeManager, _, mapperDao, driver) = prepare(List(PersonEntity, CompanyEntity))
+
+		val c1 = mapperDao.link(CompanyEntity, new Company("company1") with CompanyEntity.Stored
+		{
+			val id = 100
+		})
+
+		val oldP = mapperDao.link(PersonEntity, new Person("person1", c1) with PersonEntity.Stored
+		{
+			val id = 200
+		})
+
+		val sqls = lifecycle(driver, typeManager, PersonEntity, oldP, Person("person1", c1))
+		sqls.isEmpty should be(true)
+	}
+
+	test("optimized updates for many-to-one") {
+		import CommonEntities._
+		val (typeManager, _, mapperDao, driver) = prepare(List(PersonEntity, CompanyEntity))
+
+		val c1 = mapperDao.link(CompanyEntity, new Company("company1") with CompanyEntity.Stored
+		{
+			val id = 100
+		})
+
+		val oldP = mapperDao.link(PersonEntity, new Person("person1", c1) with PersonEntity.Stored
+		{
+			val id = 200
+		})
+
+		val sqls = lifecycle(driver, typeManager, PersonEntity, oldP, Person("changed", c1))
+		sqls.map(_.sql).toSet should be(Set(
+			"""
+			  |update Person
+			  |set name = ?
+			  |where id = ?
+			""".stripMargin.trim))
+	}
+
 	test("optimized updates for one to many and declared keys") {
+		import OneToManyDeclarePrimaryKeysSuite._
 		val (typeManager, jdbc, mapperDao, driver) = prepare(List(HouseEntity, PersonEntity))
 		val sw = mapperDao.link(PostCodeEntity, new PostCode("SW") with PostCodeEntity.Stored
 		{
@@ -53,21 +94,7 @@ class CmdToDatabaseSuite extends FunSuite with ShouldMatchers with EasyMockSugar
 			val id = 10
 		}
 
-		val oldVM = ValuesMap.fromType(typeManager, PersonEntity.tpe, oldP)
-		val newVM = ValuesMap.fromType(typeManager, PersonEntity.tpe, newP)
-
-		val cp = new CmdPhase(typeManager)
-		val cmds = cp.toUpdateCmd(PersonEntity.tpe, oldVM, newVM, uc)
-
-		val pp = new PriorityPhase(uc)
-		val pri = pp.prioritise(PersonEntity.tpe, cmds)
-
-		val cmdList = (pri.high.flatten ::: pri.low)
-		val ctb = new CmdToDatabase(uc, driver, typeManager, pri)
-		val sqls = cmdList.map {
-			cmd =>
-				ctb.toSql(cmd)
-		}.flatten
+		val sqls = lifecycle(driver, typeManager, PersonEntity, oldP, newP)
 
 		val sql = sqls.map(s => jdbc.toString(s.sql, s.values)).mkString("\n")
 		sql should be(
@@ -77,5 +104,29 @@ class CmdToDatabaseSuite extends FunSuite with ShouldMatchers with EasyMockSugar
 			  |where ((address = 'old address') and (postcode_id = 1000)) and (person_id = 10)
 			  |
 			""".stripMargin.trim)
+	}
+
+	def lifecycle[ID, PC <: Persisted, T](
+		driver: Driver,
+		typeManager: TypeManager,
+		entity: Entity[ID, PC, T],
+		oldP: T with PC,
+		newP: T
+		): List[driver.sqlBuilder.Result] = {
+		val oldVM = ValuesMap.fromType(typeManager, entity.tpe, oldP)
+		val newVM = ValuesMap.fromType(typeManager, entity.tpe, newP)
+
+		val cp = new CmdPhase(typeManager)
+		val cmds = cp.toUpdateCmd(entity.tpe, oldVM, newVM, uc)
+
+		val pp = new PriorityPhase(uc)
+		val pri = pp.prioritise(entity.tpe, cmds)
+
+		val cmdList = (pri.high.flatten ::: pri.low)
+		val ctb = new CmdToDatabase(uc, driver, typeManager, pri)
+		cmdList.map {
+			cmd =>
+				ctb.toSql(cmd)
+		}.flatten.asInstanceOf[List[driver.sqlBuilder.Result]]
 	}
 }
