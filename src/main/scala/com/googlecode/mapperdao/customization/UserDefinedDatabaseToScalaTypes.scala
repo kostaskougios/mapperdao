3aa29fb895fe340a97012b492f36e9bcef82fabb
kostas.kougios@gmail.com <sb3Xk9Eq5Nm2>
2013-03-30 10:25:18 +0000

    impl type conversions extension to allow client code to modify data types back and forth from scala to db

--- src/main/scala/com/googlecode/mapperdao/customization/UserDefinedDatabaseToScalaTypes.scala
+++ src/main/scala/com/googlecode/mapperdao/customization/UserDefinedDatabaseToScalaTypes.scala
@@ -5,6 +5,8 @@ import com.googlecode.mapperdao.SimpleColumn
 /**
  * allows custom mapping from scala -> database and from database->scala types
  *
+ * example: CustomTypesSuite
+ *
  * @author: kostas.kougios
  *          Date: 30/03/13
  */
@@ -19,7 +21,23 @@ abstract class UserDefinedDatabaseToScalaTypes extends CustomDatabaseToScalaType
 
 	def transformValuesAfterSelecting(column: SimpleColumn, v: Any) = databaseToScala((column, v))
 
+	/**
+	 * converts a scala type to a database type.
+	 *
+	 * @param data      a tuple with the column and value that needs to be converted.
+	 *
+	 * @return          a tuple with both the column and value converted. Can be same
+	 *                  as data if no convertion must occur. Note: if the type of the
+	 *                  value chances, then simpleColumn.tpe must reflect the new
+	 *                  class of the value.
+	 */
 	def scalaToDatabase(data: (SimpleColumn, Any)): (SimpleColumn, Any)
 
+	/**
+	 * converts a database type to scala type. This is the reverse function of scalaToDatabase
+	 *
+	 * @param data      as they are loaded from the database
+	 * @return          the converted value (or the same value if no convertion is applicable)
+	 */
 	def databaseToScala(data: (SimpleColumn, Any)): Any
 }
--- src/test/scala/com/googlecode/mapperdao/CustomTypesSuite.scala
+++ src/test/scala/com/googlecode/mapperdao/CustomTypesSuite.scala
@@ -19,25 +19,50 @@ class CustomTypesSuite extends FunSuite with ShouldMatchers
 	val database = s.database
 	if (database == "h2") {
 		val properties = s.loadJdbcProperties
+		/**
+		 * we need to customize the data types for DatesEntity. We can't use one of the standard
+		 * factory methods to create the mapperDao classes. We'll create each class separatelly.
+		 */
 		val dataSource = BasicDataSourceFactory.createDataSource(properties)
 		val typeRegistry = TypeRegistry(DatesEntity)
+
+		/**
+		 * we need to store Dates.time as long in the database. We'll configure
+		 * mapperdao with a UserDefinedDatabaseToScalaTypes which does the conversions
+		 */
 		val myDatabaseToScalaTypes = new UserDefinedDatabaseToScalaTypes
 		{
 			def scalaToDatabase(data: (SimpleColumn, Any)) = data match {
+				/**
+				 * we'll convert the time field to a long. We need to inspect column to see
+				 * if we're converting for the right entities (otherwise all DateTime fields
+				 * for all entities will be converted)
+				 */
 				case (column: Column, d: DateTime) if (column.entity == DatesEntity) =>
 					(column.copy(tpe = classOf[Long]), d.getMillis)
+
+				/**
+				 * for every other value, just return it unmodified
+				 */
 				case v =>
 					v
 			}
 
 			def databaseToScala(data: (SimpleColumn, Any)) =
 				data match {
-					// make sure we do the conversion only for the correct entity
+					/**
+					 * The reverse conversion. From database value => scala value.
+					 * We need to make sure we do the conversion only for the correct entity.
+					 */
 					case (column, l: Long) if (column.entity == DatesEntity) =>
 						new DateTime(l)
 					case (column, value) => value
 				}
 		}
+
+		/**
+		 * now we can initialize mapperDao
+		 */
 		val (jdbc, mapperDao, queryDao, _) = Setup.create(Database.byName(database), dataSource, typeRegistry, customDatabaseToScalaTypes = myDatabaseToScalaTypes)
 
 		test("date as long in db") {
