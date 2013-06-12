df58f7df133d9700c872bc45c2a2adea1cdc39b0
Konstantinos Kougios <kostas.kougios@googlemail.com>
2013-06-12 20:56:45 +0100

    DataValues to contain data for related

--- src/test/scala/com/googlecode/mapperdao/RelatedAreLoadedFromDatabaseValuesSuite.scala
+++ src/test/scala/com/googlecode/mapperdao/RelatedAreLoadedFromDatabaseValuesSuite.scala
@@ -106,4 +106,29 @@ class RelatedAreLoadedFromDatabaseValuesSuite extends FunSuite with ShouldMatche
 		Helpers.intIdOf(r.owns.head) should be(201)
 		Helpers.intIdOf(r.owns.tail.head) should be(202)
 	}
+
+	test("one-to-one") {
+		val ids = Helpers.idToList(30)
+		val dbVs = new DatabaseValues(Map(
+			"id" -> 30,
+			"name" -> "husband",
+			"age" -> 20,
+			"wife_id" -> 301
+		), Map(
+			HusbandEntity.wife.column.aliasLowerCase -> (
+				new DatabaseValues(
+					Map(
+						"id" -> 301,
+						"name" -> "wife",
+						"age" -> 19
+					)
+				) :: Nil
+				)
+		)
+		)
+		val r = mapperDao.selectInner(HusbandEntity, SelectConfig.default, ids, new EntityMap, Some(dbVs)).get
+		r should be(Husband("husband", 20, Wife("wife", 19)))
+		Helpers.intIdOf(r) should be(30)
+		Helpers.intIdOf(r.wife) should be(301)
+	}
 }
