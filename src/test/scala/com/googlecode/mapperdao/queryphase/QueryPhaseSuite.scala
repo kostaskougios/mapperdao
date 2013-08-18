800c88b0d8a1c9c9e4d65291c4afbd9f5e6fb970
kostas.kougios@gmail.com <sb3Xk9Eq5Nm2>
2013-08-18 20:01:39 +0100

    rc24

--- src/test/scala/com/googlecode/mapperdao/queryphase/QueryPhaseSuite.scala
+++ src/test/scala/com/googlecode/mapperdao/queryphase/QueryPhaseSuite.scala
@@ -1,37 +1,37 @@
-package com.googlecode.mapperdao.queryphase
-
-import org.scalatest.FunSuite
-import org.scalatest.matchers.ShouldMatchers
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import com.googlecode.mapperdao.queryphase.model._
-
-/**
- * @author kkougios
- */
-@RunWith(classOf[JUnitRunner])
-class QueryPhaseSuite extends FunSuite with ShouldMatchers
-{
-
-	import com.googlecode.mapperdao.CommonEntities._
-
-	test("many-to-many") {
-		val qp = new QueryPhase
-		val q = qp.toQuery(ProductEntity.tpe)
-
-		q should be(
-			Select(
-				From(
-					InQueryTable(Table(ProductEntity.tpe.table), "maint")
-				),
-				List(
-					Join(
-						Table(ProductEntity.attributes.column.linkTable),
-						NoClause
-					)
-				),
-				NoClause
-			)
-		)
-	}
-}
+//package com.googlecode.mapperdao.queryphase
+//
+//import org.scalatest.FunSuite
+//import org.scalatest.matchers.ShouldMatchers
+//import org.junit.runner.RunWith
+//import org.scalatest.junit.JUnitRunner
+//import com.googlecode.mapperdao.queryphase.model._
+//
+///**
+// * @author kkougios
+// */
+//@RunWith(classOf[JUnitRunner])
+//class QueryPhaseSuite extends FunSuite with ShouldMatchers
+//{
+//
+//	import com.googlecode.mapperdao.CommonEntities._
+//
+//	test("many-to-many") {
+//		val qp = new QueryPhase
+//		val q = qp.toQuery(ProductEntity.tpe)
+//
+//		q should be(
+//			Select(
+//				From(
+//					InQueryTable(Table(ProductEntity.tpe.table), "maint")
+//				),
+//				List(
+//					Join(
+//						Table(ProductEntity.attributes.column.linkTable),
+//						NoClause
+//					)
+//				),
+//				NoClause
+//			)
+//		)
+//	}
+//}
