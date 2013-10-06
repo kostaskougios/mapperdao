f1a4c570789015b76c0922e20371e57e479fb422
kostas.kougios@gmail.com <sb3Xk9Eq5Nm2>
2013-10-06 17:21:48 +0100

    scala 2.10.3

--- src/main/scala/com/googlecode/mapperdao/queryphase/model/OnClause.scala
+++
@@ -1,7 +0,0 @@
-package com.googlecode.mapperdao.queryphase.model
-
-/**
- * @author: kostas.kougios
- *          Date: 16/08/13
- */
-case class OnClause(left: List[Column], right: List[Column])
--- src/main/scala/com/googlecode/mapperdao/queryphase/model/WhereValueComparisonClause.scala
+++
@@ -1,7 +0,0 @@
-package com.googlecode.mapperdao.queryphase.model
-
-/**
- * @author: kostas.kougios
- *          Date: 28/08/13
- */
-case class WhereValueComparisonClause(left: Column, operator: String, right: String) extends Clause
